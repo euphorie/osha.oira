5591d8f1fc8da18e56379a2dfe97409f0161519f
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2010-08-10 11:35:39 +0000

    EU-OSHA customisations for Euphorie/OiRA

---
+++ osha/oira/README.txt
@@ -0,0 +1,14 @@
+osha.oira Package Readme
+=========================
+
+Overview
+--------
+
+'EU-OSHA customisations for Euphorie/OiRA'
+
+
+Your tests here
+---------------
+
+    >>> 1 + 1
+    3
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='osha.oira',
+      version=version,
+      description="'EU-OSHA customisations for Euphorie/OiRA'",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='syslab.com',
+      author_email='thomas@syslab.com',
+      url="'http://www.oira.osha.europa.eu/'",
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['osha'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

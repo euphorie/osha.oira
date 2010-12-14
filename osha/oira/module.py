2218d13478cd8944489c37d0661fe491d0524908
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2010-12-14 16:26:31 +0000

    Add custom edit and add forms for Module. Add a custom label property for these to enable submodules to be identified.

---
+++ osha/oira/module.py
@@ -0,0 +1,35 @@
+from five import grok
+from zope.component import getUtility
+from plone.directives import dexterity
+from plone.dexterity.interfaces import IDexterityFTI
+from euphorie.content import MessageFactory as _
+from euphorie.content.module import IModule
+
+class Edit(dexterity.EditForm):
+    grok.context(IModule)
+
+    @property
+    def label(self):
+        if self.context.aq_parent.portal_type  == 'euphorie.module':
+            type_name = _('Submodule')
+        else:
+            portal_type = self.context.portal_type
+            fti = getUtility(IDexterityFTI, name=portal_type)
+            type_name = fti.Title()
+        return _(u"Edit ${name}", mapping={'name': type_name})
+
+
+class Add(dexterity.AddForm):
+    grok.name('euphorie.module')
+    grok.context(IModule)
+
+    @property
+    def label(self):
+        if self.context.portal_type  == 'euphorie.module':
+            type_name = _('Submodule')
+        else:
+            portal_type = self.portal_type
+            fti = getUtility(IDexterityFTI, name=portal_type)
+            type_name = fti.Title()
+        return _(u"Add ${name}", mapping={'name': type_name})
+

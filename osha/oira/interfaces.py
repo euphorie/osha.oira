b4ca79b887b2e32acbb8dc9ff6f8d78c096a5717
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2010-12-15 14:00:06 +0000

    Add new browserlayer interface

--- osha/oira/interfaces.py
+++ osha/oira/interfaces.py
@@ -1,4 +1,8 @@
 from euphorie.client.interfaces import IReportPhaseSkinLayer
+from euphorie.client.interfaces import IClientSkinLayer
 
 class IOSHAReportPhaseSkinLayer(IReportPhaseSkinLayer):
     """Skin layer used during the action plan phase."""
+
+class IOSHAClientSkinLayer(IClientSkinLayer):
+    """Marker interface for the TNO client skin."""

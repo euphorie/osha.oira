8aceae9726535faa0847cbca477b05a5072efc90
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2011-02-28 15:34:37 +0000

    #2681 Remove header capitalization for Greek.
    - Changed the LEGAL... headling in webhelpder.pt to add a language specific
      class.
    - Added CSS in main.css that will remove the uppercasing if the language is
      'el'
    - Added helper method in risk.py to get the current language.
    - Removed the explicit linking to main.css in report_evaluation.pt and
      report_actionplan.pt and instead put the linking in webhelpers.pt's css
      macro.

--- osha/oira/risk.py
+++ osha/oira/risk.py
@@ -1,17 +1,32 @@
+from Acquisition import aq_inner
+from zope.component import getMultiAdapter
 from five import grok
 from euphorie.client import risk
 import interfaces
 
 grok.templatedir("templates")
 
-class OSHAIdentificationView(risk.IdentificationView):
+class OSHAMixin():
+
+    def get_language(self):
+        context = aq_inner(self.context)
+        portal_state = getMultiAdapter(
+                                (context, self.request), 
+                                name=u'plone_portal_state'
+                                )
+        return portal_state.language()
+
+
+class OSHAIdentificationView(risk.IdentificationView, OSHAMixin):
     grok.layer(interfaces.IOSHAIdentificationPhaseSkinLayer)
     grok.template("risk_identification")
 
-class OSHAEvaluationView(risk.EvaluationView):
+
+class OSHAEvaluationView(risk.EvaluationView, OSHAMixin):
     grok.layer(interfaces.IOSHAEvaluationPhaseSkinLayer)
     grok.template("risk_evaluation")
 
-class OSHAActionPlanView(risk.ActionPlanView):
+
+class OSHAActionPlanView(risk.ActionPlanView, OSHAMixin):
     grok.layer(interfaces.IOSHAActionPlanPhaseSkinLayer)
     grok.template("risk_actionplan")

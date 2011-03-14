c513e5d35518afac478aca9bed99d8c0ce8d4cc5
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2011-03-14 19:11:45 +0000

    #2724
    Add custom Delete actions for sectors and surveygroups, to prevent them from
    being deleted if they contain any published surveys.

--- osha/oira/sector.py
+++ osha/oira/sector.py
@@ -1,9 +1,15 @@
 from five import grok
+from Acquisition import aq_parent
 from zope.component import getUtility
 from z3c.appconfig.interfaces import IAppConfig
-from euphorie.content import sector
 from plone.directives import dexterity
+from Products.statusmessages.interfaces import IStatusMessage
 from plonetheme.nuplone.skin.interfaces import NuPloneSkin
+from plonetheme.nuplone.skin import actions
+from plonetheme.nuplone.utils import getPortal
+from euphorie.content import sector
+from euphorie.content import MessageFactory as _
+from euphorie.content.survey import ISurvey
 
 grok.templatedir("templates")
 
@@ -36,5 +42,39 @@ class SectorAdd(dexterity.AddForm):
 class View(sector.View):
     grok.template("sector_view")
     grok.name("nuplone-view")
+
+
+class Delete(actions.Delete):
+    """ Only delete the sector if it doesn't have any published surveys.
+    """
+    grok.context(sector.ISector)
+
+    def verify(self, container, context):
+        flash = IStatusMessage(self.request).addStatusMessage
+        sector = context
+        country = container
+        client = getPortal(container).client
+
+        if country.id not in client:
+            return True
+
+        cl_country = client[country.id]
+        if sector.id not in cl_country:
+            return True
+
+        # Look for any published surveys in the client sector, and prevent
+        # deletion if any are found
+        cl_sector = cl_country[sector.id]
+        surveys = [s for s in cl_sector.values() if s.id != 'preview']
+        if surveys:
+            flash(
+                _("message_not_delete_published_sector", 
+                default=u"You can not delete a sector that contains published OiRA tools."), 
+                "error"
+                )
+            self.request.response.redirect(context.absolute_url())
+            return False
+        return True
+
 
 
--- osha/oira/surveygroup.py
+++ osha/oira/surveygroup.py
@@ -1,6 +1,11 @@
 from five import grok
-from euphorie.content import surveygroup
+from Acquisition import aq_parent
+from Products.statusmessages.interfaces import IStatusMessage
 from plonetheme.nuplone.skin.interfaces import NuPloneSkin
+from plonetheme.nuplone.skin import actions
+from plonetheme.nuplone.utils import getPortal
+from euphorie.content import MessageFactory as _
+from euphorie.content import surveygroup
 from euphorie.content.survey import ISurvey
 
 grok.templatedir("templates")
@@ -18,4 +23,41 @@ class View(surveygroup.View):
         return templates
 
 View.render = None
+
+
+class Delete(actions.Delete):
+    """ Only delete the surveygroup if it doesn't have a published version.
+    """
+    grok.context(surveygroup.ISurveyGroup)
+
+    def verify(self, container, context):
+        flash = IStatusMessage(self.request).addStatusMessage
+        surveygroup = context
+        sector = container
+        country = aq_parent(sector)
+        client = getPortal(container).client
+
+        if country.id not in client:
+            return True
+
+        cl_country = client[country.id]
+        if sector.id not in cl_country:
+            return True
+
+        cl_sector = cl_country[sector.id]
+        if surveygroup.id not in cl_sector:
+            return True
+
+        count = 0
+        surveys = [s for s in cl_sector[surveygroup.id].values() if s.id != 'preview']
+        if surveys:
+            flash(
+                _("message_not_delete_published_surveygroup", 
+                default=u"You can not delete an OiRA tool that has been published."), 
+                "error"
+                )
+            self.request.response.redirect(context.absolute_url())
+            return False
+        return True
+
 

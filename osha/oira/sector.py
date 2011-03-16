0c40a29298793c645c3233cc6bc994e859b1d359
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2011-03-16 19:36:32 +0000

    #2560 Added a new property for the Sector, "statistics_level"

--- osha/oira/sector.py
+++ osha/oira/sector.py
@@ -1,17 +1,40 @@
 from five import grok
-from Acquisition import aq_parent
+from zope import schema
 from zope.component import getUtility
+from zope.interface import alsoProvides
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
 from z3c.appconfig.interfaces import IAppConfig
+from plone.app.dexterity.behaviors.metadata import DCFieldProperty
+from plone.app.dexterity.behaviors.metadata import MetadataBase
 from plone.directives import dexterity
-from Products.statusmessages.interfaces import IStatusMessage
+from plone.directives import form
+from plone.autoform.interfaces import IFormFieldProvider
 from plonetheme.nuplone.skin.interfaces import NuPloneSkin
 from plonetheme.nuplone.skin import actions
 from plonetheme.nuplone.utils import getPortal
+from Products.statusmessages.interfaces import IStatusMessage
 from euphorie.content import sector
 from euphorie.content import MessageFactory as _
-from euphorie.content.survey import ISurvey
 
 grok.templatedir("templates")
+
+class IOSHASector(form.Schema):
+    """ """
+    statistics_level = schema.Choice(
+            title = _("label_statistics_level", default=u"Statistics Level"),
+            required=True,
+            vocabulary = SimpleVocabulary([
+                            SimpleTerm(1, title=u"1"),
+                            SimpleTerm(2, title=u"2"),
+                            ]),
+            default=1,
+            )
+
+alsoProvides(IOSHASector, IFormFieldProvider)
+
+class OSHASector(MetadataBase):
+    statistics_level = DCFieldProperty(IOSHASector['statistics_level'])
 
 class Settings(sector.Settings):
     """ Override so that we can use our own template
@@ -19,7 +42,7 @@ class Settings(sector.Settings):
     grok.template("settings")
 
 class SectorAdd(dexterity.AddForm):
-    grok.context(sector.ISector)
+    grok.context(IOSHASector)
     grok.name('euphorie.sector')
     grok.require("cmf.ModifyPortalContent")
     grok.layer(NuPloneSkin)

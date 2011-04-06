5fa1556fa18514e877b4e516f7ee36728e9dc04c
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2011-04-06 11:15:33 +0000

    #2855: Add a disclaimer page.

---
+++ osha/oira/disclaimer.py
@@ -0,0 +1,15 @@
+import logging
+from zope.interface import Interface
+from five import grok
+from interfaces import IOSHAClientSkinLayer
+
+log = logging.getLogger(__name__)
+grok.templatedir("templates")
+
+class Disclaimer(grok.View):
+    grok.context(Interface)
+    grok.layer(IOSHAClientSkinLayer)
+    grok.name("disclaimer")
+    grok.require("zope2.View")
+    grok.template("disclaimer")
+

dbc49e66cb02886d1be1be324c834da0f1e5dca8
Wolfgang Thomas <thomas@syslab.com>
2011-05-09 12:09:47 +0200

    more sensible version of this script

--- src/osha/oira/locales/findDirtyTranslations.py
+++ src/osha/oira/locales/findDirtyTranslations.py
@@ -51,28 +51,34 @@ outpo.header = newpo.header
 
 counter = 0
 
-for entry in oldpo:
-    target = newpo.find(entry.msgid)
-    if not target:
-        print "WARNING! msgid '%s' not present in %s." % (entry.msgid, newfile)
-        continue
-    match_old = patt.match(entry.comment)
-    if match_old:
-        default_old = match_old.group(1).replace('\n', ' ')
-    else:
-        print "WARNING! msgid '%s' in 'old' file does not have a default " \
-            "translation." % entry.msgid
-        continue
-    default_new = u''
-    match_new = patt.match(target.comment)
+# look at every entry in the new (POT) file
+for entry in newpo:
+    default_old = default_new = u''
+    # fist, extract the default translation of the new (POT) file
+    match_new = patt.match(entry.comment)
     if match_new:
         default_new = match_new.group(1).replace('\n', ' ')
-        if "Default:" in default_new:
-            print "ERROR! There seems to be a duplicate Default entry for msgid '%s'" % entry.msgid
-    if default_old != default_new:
-        outpo.append(polib.POEntry(msgid=target.msgid, msgstr=default_new,
-            occurrences=target.occurrences, comment=target.comment))
+    else:
+        print "WARNING! msgid '%s' in 'new' file does not have a default " \
+            "translation." % entry.msgid
+        continue
+    # try to find the same message in the existing po file
+    target = oldpo.find(entry.msgid)
+    if not target:
+        # not found == new translation
+        outpo.append(polib.POEntry(msgid=entry.msgid, msgstr=default_new,
+            occurrences=entry.occurrences, comment=entry.comment))
         counter += 1
+    else:
+        match_old = patt.match(target.comment)
+        if match_old:
+            default_old = match_old.group(1).replace('\n', ' ')
+            if "Default:" in default_old:
+                print "ERROR! There seems to be a duplicate Default entry for msgid '%s'" % entry.msgid
+        if default_old != default_new:
+            outpo.append(polib.POEntry(msgid=entry.msgid, msgstr=default_new,
+                occurrences=entry.occurrences, comment=entry.comment))
+            counter += 1
 
 outpo.save(outfile)
 

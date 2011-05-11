4d4361588245e8a4374678a90aee13c7eb0c8ad2
Wolfgang Thomas <thomas@syslab.com>
2011-05-11 15:15:34 +0200

    new "dummy" file that only holds msgids and their default translations, so that they will get picked up by the i18n extraction engine

---
+++ src/osha/oira/manual_translations.py
@@ -0,0 +1,39 @@
+# Dont import this file, it's just for our i18n toolchain
+
+
+from euphorie.content import MessageFactory as _
+
+_("help_measure_action_plan", default=u"Describe your general approach to "
+    u"eliminate or (if the risk is not avoidable) reduce the risk.")
+_("help_measure_prevention_plan", default=u"Describe the specific action(s) "
+    u"required to implement this approach (to eliminate or to reduce the "
+    u"risk).")
+_("help_measure_requirements", default=u"Describe: 1) what is your general "
+    u"approach to eliminate or (if  the risk is not avoidable) reduce the "
+    u"risk; 2) the specific action(s) required to implement this approach "
+    u"(to eliminate or to reduce the risk); 3) the level of expertise needed "
+    u"to implement the measure, for instance \"common sense (no OSH knowledge "
+    u"required)\", \"no specific OSH expertise, but minimum OSH knowledge or "
+    u"training and/or consultation of OSH guidance required\", or \"OSH "
+    u"expert\". You can also describe here any other additional requirement "
+    u"(if any).")
+_("Solution", default=u"Measure")
+_("A solution.", default=u"A standard measure for a risk.")
+_("header_solutions", default=u"Standard measures")
+_("risk_solution_header", default=u"Measure ${number}")
+_("nav_surveys", default=u"OiRA Tools")
+_("Survey", default=u"OiRA Tool")
+_("help_header_sessions", default=u"OiRA Tools")
+_("label_sessions", default=u"OiRA Tools")
+_("Survey import", default=u"OiRA Tool import")
+_("Import survey version", default=u"Import OiRA tool version")
+_("Survey version", default=u"OiRA Tool version")
+_("intro_add_surveygroup", default=u"The form will allow you to create a new "
+    u"OiRA Tool.")
+_("survey_source_other", default=u"Base my new OiRA Tool on an existing OiRA "
+    u"Tool of another organisation.")
+_("survey_source_local", default=u"Base my new OiRA Tool on an existing OiRA "
+    u"Tool of my organisation.")
+_("survey_source_scratch", default=u"Create a new OiRA Tool from scratch.")
+
+

5eb6cc1a0fffdb69c7a584df6517fa318e9a05cc
Wolfgang Thomas <thomas@syslab.com>
2011-05-23 18:31:16 +0200

    more manual translations. I found them by looking at what msgids would be deleted in the already existing po files. For every msgid still present in EUPHORIE with a different default translation than in the po file, I added it to this manual file.

--- src/osha/oira/manual_translations.py
+++ src/osha/oira/manual_translations.py
@@ -35,5 +35,117 @@ _("survey_source_other", default=u"Base my new OiRA Tool on an existing OiRA "
 _("survey_source_local", default=u"Base my new OiRA Tool on an existing OiRA "
     u"Tool of my organisation.")
 _("survey_source_scratch", default=u"Create a new OiRA Tool from scratch.")
-
-
+_("no_profile_questions", default=u"This OiRA Tool has no profile questions.")
+_("button_create_new", default=u"Create new OiRA Tool")
+_("message_choose_country", default=u"Please choose a country of which you "
+    u"would like to view or copy an OiRA Tool.")
+_("intro_select_sector", default="Select a sector for which you would like to "
+    u"view or copy an OiRA Tool.")
+_("help_survey_title", default=u"This is the title of this OiRA Tool "
+    u"version. This name is never shown to users.")
+_("help_introduction", default=u"The introduction text is shown when starting "
+    u"a new OiRA Tool session. If no introduction is provided here a standard "
+    u"text will be shown. Please keep this text brief so it will easily fit "
+    u"on screens of small devices such as phones and PDAs.")
+_("message_delete_no_last_survey", default=u"You cannot delete the only OiRA "
+    u"Tool version.")
+_("label_surveygroup_title", default=u"Title of imported OiRA Tool")
+_("upload_success", default=u"Succesfully imported the OiRA Tool")
+_("title_import_sector_survey", default=u"Import sector and OiRA Tool")
+_("intro_add_survey", default=u"The form will allow you to create a new OiRA "
+    u"Tool revision.")
+_("label_choose_survey", default=u"Choose a revision of the selected OiRA "
+    u"Tool")
+_("label_choose_surveygroup", default=u"Choose an OiRA Tool")
+_("help_risks_present", default=u"Now that you have identified the existing "
+    u"hazards/problems in your company/organisation you need to evaluate the "
+    u"risks arising from them.")
+_("hader_company_details", default=u"You are about to print the report... "
+    u"please complete the optional form and then proceed.")
+_("company_details_intro", default=u"You are free to provide the information "
+    u"below. This information is meant to improve the tools and the services "
+    u"provided. This data will not appear in the report.")
+_("label_solution_direction", default=u"Solution")
+_("Succesfully published the survey", default=u"Succesfully published the "
+    u"OiRA Tool")
+_("Succesfully created a preview for the survey. It can be accessed at "
+    u"${url} .", default=u"Succesfully created a preview for the OiRA Tool. "
+    u"It can be accessed at ${url}.")
+_("help_publish", default=u"Publish the selected OiRA Tool live with its "
+    u"latest changes.")
+_("help_add_version", default=u"Create a duplicate of the selected OiRA Tool")
+_("help_evaluation_optional", default=u"The option allows the end-user to "
+    u"skip the Evaluation step.")
+_("help_profilequestion_question", default=u"If this is to be \"optional\", "
+    u"it must be formulated so that it is answerable with YES (the end-user "
+    u"will have to tick a box) or NO (e.g. \"Do you work with the public?\"). "
+    u"If this is to be \"repeatable\", it must be formulated as a prompt to "
+    u"fill in multiple values (e.g. \"List your different work locations\").")
+_("description_profilequestion", default=u"The basic architecture of an "
+    u"Online interactive Risk Assessment consists of: - profile questions "
+    u"(questions defining the company profile) - modules and sub-modules - "
+    u"risks (affirmative statements). Profile questions are used to determine "
+    u"whether particular modules apply to the end user at all (OPTIONAL), or "
+    u"whether they should be repeated a number of times (REPEATABLE). Such "
+    u"questions are asked BEFORE starting the risk identification and "
+    u"evaluation. If the end-user does not confirm that an optional profile "
+    u"question is relevant (by ticking the box), then the corresponding "
+    u"module will be skipped entirely. If the end-user enters more than one "
+    u"option to a repeatable profile question then the corresponding module "
+    u"will be repeated the specified number of times.")
+_("help_module_description", default=u"Include any relevant information that "
+    u"may be helpful for the end-user.")
+_("help_module_optional", default=u"Allows the end-user to skip this module "
+    u"and everything inside it.")
+_("help_module_question", default=u"The question to ask the end-user if this "
+    u"module is optional. It must be formulated so that it is answerable with "
+    u"YES (the end-user will have to tick a box) or NO")
+_("help_solution_direction", default=u"This information will appear in the "
+    u"Action plan step and should include an overview of general solution(s) "
+    u"related to this module.")
+_("label_statement", default=u"Affirmative statement")
+_("label_problem_description", default=u"Negative statement")
+_("help_statement", default=u"This is a short affirmative statement about a "
+    u"possible risk (e.g. The building is well maintained.)")
+_("help_problem_description", default=u"This is the inverse of the "
+    u"affirmative statement (e.g. The building is not well maintained.)")
+_("help_risk_description", default=u"Describe the risk. Include any relevant "
+    u"information that may be helpful for the end-user.")
+_("risktype_top5", default=u"Priority risk")
+_("help_risk_type", default=u"\"Priority risk\" is one of the high risks in "
+    u"the sector. \"Risk\" is related to the workplace or to the work carried "
+    u"out. \"Policy\" refers to agreements, procedures, and management "
+    u"decisions.")
+_("help_evaluation_method", default=u"Choose between ESTIMATED (rough "
+    u"estimation) or CALCULATED (combination of probability, frequency and "
+    u"severity) method.")
+_("help_default_priority", default=u"You can help the end-user by selecting a "
+    u"default priority. He/she can still change the priority.")
+_("help_default_severity", default=u"Indicate the severity if this risk "
+    u"occurs.")
+_("help_help_introduction", default=u"General information on risk assessment")
+_("help_authentication", default=u"This text should explain how to register "
+    u"and login.")
+_("help_sessions", default=u"This text should describe the main functions of "
+    u"the OiRA Tool.")
+_("help_preparation", default=u"This text should explain the 2 types of "
+    u"profile questions.")
+_("help_identification", default=u"This text should explain how the risk "
+    u"identification works.")
+_("help_evaluation", default=u"This text should explain how to evaluate the "
+    u"identified risks.")
+_("help_actionplan", default=u"This text should explain how to fill in the "
+    u"Action plan.")
+_("help_reports", default=u"This text should describe how the report can "
+    u"either be saved or printed.")
+_("help_finalwords", default=u"General final recommendations.")
+_("intro_print_identification_1", default=u"It is possible for you to print "
+    u"out the contents of the tool to enable you to look for the information "
+    u"at the relevant workplace (by walkink around and looking at what could "
+    u"cause harm, or by consulting the workers about problems they have "
+    u"encountered). You can then introduce the information into the tool.")
+_("intro_print_identification_2", default=u"You may also wish to distribute "
+    u"the contents among the workers and invite them to participate in "
+    u"identifying the hazards/problems, evaluating the risks and deciding on "
+    u"preventive action. Their feedback can be taken into account when "
+    u"filling in this tool")

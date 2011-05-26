a23b5fe70af277b6a32f3a61731d2468df6e3353
JC Brand <jc@opkode.com>
2011-05-26 13:27:07 +0200

    3048 Fixed the 'Show' 'Hide' mechanism to be able to translate those strings.

--- src/osha/oira/browser/javascripts/oira.js
+++ src/osha/oira/browser/javascripts/oira.js
@@ -21,17 +21,16 @@ $("button#report_comment_submit").click(function(event) {
     ); 
 });
 
-$("a#oira_legal_show_hide_anchor").click(function(event) {
+$("a#oira_legal_hide_anchor").click(function(event) {
     event.preventDefault();
-    var anchor = jQuery("a#oira_legal_show_hide_anchor");
-    var div = jQuery("div#legal_reference");
-    var current_txt = anchor.text() 
-    if (current_txt == 'Show') {
-        anchor.text('Hide');
-        div.show(200);
-    }
-    else {
-        anchor.text('Show');
-        div.hide(700);
-    }
+    jQuery("a#oira_legal_hide_anchor").hide();
+    jQuery("a#oira_legal_show_anchor").show();
+    jQuery("div#legal_reference").show(200);
+});
+
+$("a#oira_legal_show_anchor").click(function(event) {
+    event.preventDefault();
+    jQuery("a#oira_legal_hide_anchor").show();
+    jQuery("a#oira_legal_show_anchor").hide();
+    jQuery("div#legal_reference").hide(200);
 });

6e5047d632a26891848e17a39d9b680dd852ef45
JC Brand <jc@opkode.com>
2011-06-08 09:59:25 +0200

    Add --ignore-translated option.

--- src/osha/oira/locales/getNewEntries.py
+++ src/osha/oira/locales/getNewEntries.py
@@ -5,10 +5,12 @@
 second file that are not in the first one. These entries are then written to a
 new po file.
 
-usage:          %(program)s first.po second.pot out.po
-first.po        A po/pot file with
-second.pot      A po/pot file with updated default translations (e.g. via extraction)
-out.po          A name for the output po file
+usage:                  %(program)s first.po second.pot out.po
+first.po                A po/pot file with
+second.pot              A po/pot file with updated default translations (e.g. via extraction)
+out.po                  A name for the output po file
+--ignore-translated     Ignore translated entries. Only untranslated and fuzzy
+                        entries will be returned.
 """
 
 import sys
@@ -25,6 +27,8 @@ from findDirtyTranslations import append_entry
 def main():
     if len(sys.argv) < 4:
         usage(sys.stderr, "\nERROR: Not enough arguments")
+    elif len(sys.argv) > 5:
+        usage(sys.stderr, "\nERROR: Too many arguments")
 
     oldfile = sys.argv[1]
     if not os.path.isfile(oldfile):
@@ -44,7 +48,16 @@ def main():
     outpo.header = secondpo.header
     [outpo.metadata.update({key: val}) for (key, val) in secondpo.metadata.items()]
 
-    for entry in secondpo:
+    if len(sys.argv) == 5 and "--ignore-translated" in sys.argv[4]:
+        entries = secondpo.untranslated_entries() + secondpo.fuzzy_entries()
+    else:
+        entries = secondpo
+
+    for entry in entries:
+        if entry.obsolete:
+            # Ignore outcommented entries
+            continue
+
         default= get_default(entry)
         if not firstpo.find(entry.msgid):
             outpo = append_entry(outpo, entry, default)

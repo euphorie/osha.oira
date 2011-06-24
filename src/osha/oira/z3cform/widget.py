e8cf549cd0c11ea46657b8cf08013860b9f3266d
JC Brand <jc@opkode.com>
2011-06-24 10:54:58 +0200

    1537: Only show radio buttons when field vocab is less than 7.

--- src/osha/oira/z3cform/widget.py
+++ src/osha/oira/z3cform/widget.py
@@ -3,16 +3,25 @@ from zope.interface import implementer
 from zope.schema.interfaces import IChoice
 from z3c.form.interfaces import IFieldWidget
 from z3c.form.widget import FieldWidget
+from z3c.form.browser.select import SelectWidget
 from plonetheme.nuplone.z3cform.widget import SingleRadioWidget
+from plonetheme.nuplone.z3cform.utils import getVocabulary
 from interfaces import IOiRAFormLayer
 
 @adapter(IChoice, IOiRAFormLayer)
 @implementer(IFieldWidget)
 def ChoiceWidgetFactory(field, request):
     """ #1537: OSHA wants Choice fields to all look alike and all be radio
-        butotns.
+        buttons.
 
         NuPlone on the other hand has radio buttons for items<5 and dropdown
         otherwise.
+
+        We increase min here
     """
-    return FieldWidget(field, SingleRadioWidget(request))
+    vocabulary=getVocabulary(field)
+    if vocabulary is None or len(vocabulary)>6:
+        widget=SelectWidget
+    else:
+        widget=SingleRadioWidget
+    return FieldWidget(field, widget(request))

e23f654108ff6e8f42b256909d10e7dadd0557bc
JC Brand <jc@opkode.com>
2011-07-15 11:29:54 +0200

    Add custom template report_company to add "Save and continue" button.

--- src/osha/oira/company.py
+++ src/osha/oira/company.py
@@ -2,8 +2,11 @@ from five import grok
 from euphorie.client.company import Company as GenericCompany
 from osha.oira.interfaces import IOSHAReportPhaseSkinLayer
 
+grok.templatedir("templates")
+
 class Company(GenericCompany):
     """ Override the class from euphorie.client to add our own layer.
     """
     grok.layer(IOSHAReportPhaseSkinLayer)
+    grok.template("report_company")
 

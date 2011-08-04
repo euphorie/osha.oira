e6a1fc6be84734b22ccc4c89a86e679fa99acef1
JC Brand <jc@opkode.com>
2011-08-04 15:52:23 +0200

    3343: Customize info bubble according to eval algorithm.
    Override the add/edit forms to let us dynamically change the
    evaluation_method's description field according to the risk's evaluation
    algorithm.

--- src/osha/oira/risk.py
+++ src/osha/oira/risk.py
@@ -2,7 +2,10 @@ from Acquisition import aq_inner
 from zope.component import getMultiAdapter
 from five import grok
 from euphorie.client import risk
+from euphorie.content.risk import Edit as RiskEditForm
+from euphorie.content.risk import Add as RiskAddForm
 from osha.oira import interfaces
+from osha.oira import _
 
 grok.templatedir("templates")
 
@@ -30,3 +33,50 @@ class OSHAEvaluationView(risk.EvaluationView, OSHAMixin):
 class OSHAActionPlanView(risk.ActionPlanView, OSHAMixin):
     grok.layer(interfaces.IOSHAActionPlanPhaseSkinLayer)
     grok.template("risk_actionplan")
+
+
+class OSHAFormMixin:
+    """ """
+    def setDynamicDescriptions(self):
+        """ Set the evaluation_method description depending on the evaluation
+            algorithm (Kinney or French)
+        """
+        evalgroup = self.groups[self.order.index('header_evaluation')]
+        evalfield = evalgroup.fields.get('evaluation_method', None)
+        if not evalfield:
+            return
+
+        if self.evaluation_algorithm == 'kinney':
+            evalfield.field.description = \
+                _("help_evaluation_method_kinney",
+                default="Choose between ESTIMATED (rough estimation) or "
+                        "CALCULATED (combination of probability, frequency "
+                        "and severity) method.")
+
+        elif self.evaluation_algorithm == 'french':
+            evalfield.field.description = \
+                _("help_evaluation_method_french",
+                default="Choose between ESTIMATED (rough estimation) or "
+                        "CALCULATED (combination of frequency "
+                        "and severity) method.")
+                
+
+class Add(RiskAddForm, OSHAFormMixin):
+    """ Override to allow us to dynamically set field descriptions
+    """
+    grok.layer(interfaces.IOSHAContentSkinLayer)
+
+    def updateFields(self):
+        super(Add, self).updateFields()
+        self.setDynamicDescriptions()
+
+
+class Edit(RiskEditForm, OSHAFormMixin):
+    """ Override to allow us to dynamically set field descriptions
+    """
+    grok.layer(interfaces.IOSHAContentSkinLayer)
+
+    def updateFields(self):
+        super(Edit, self).updateFields()
+        self.setDynamicDescriptions()
+

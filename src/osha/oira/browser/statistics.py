84cda86e39a060b78cc4065502043495833d3d90
Manuel Reinhardt <reinhardt@syslab.com>
2012-01-19 15:38:02 +0000

    migrated to grok, added show-statistics view

--- src/osha/oira/browser/statistics.py
+++ src/osha/oira/browser/statistics.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from AccessControl import Unauthorized
+from five import grok
 
 from z3c.saconfig import Session
 from zope.sqlalchemy import datamanager
@@ -10,21 +12,22 @@ import transaction
 
 from Products.CMFPlone.utils import safe_unicode
 import re
+import urlparse, urllib, urllib2
 
 logger = logging.getLogger("osha.oira/browser.statistics")
 
-class WriteStatistics(BrowserView):
+class WriteStatistics(grok.View):
 
-    def __init__(self, request, context):
-        super(BrowserView, self).__init__(request, context)
+    grok.context(IPloneSiteRoot)
+    grok.require('cmf.ManagePortal')
+    grok.name('write-statistics')
 
-    def __call__(self):
+    def render(self):
         cat = getToolByName(self.context, 'portal_catalog')
         dbtable_surveys = 'statistics_surveys'
         dbtable_modules = 'statistics_modules'
         path_re = re.compile('/Plone2/[^/]*/(.*)')
 
-        import ipdb; ipdb.set_trace()
         published_surveys = cat(portal_type='euphorie.survey',path='/Plone2/client',sort_on='path')
         unpublished_surveys = cat(portal_type='euphorie.survey',path='/Plone2/sectors',sort_on='path')
         modules = cat(portal_type='euphorie.module',sort_on='path')
@@ -64,3 +67,27 @@ class WriteStatistics(BrowserView):
 
         from pprint import pformat
         return "Written:\n" + pformat(info_surveys) + pformat(info_modules)
+
+
+class ShowStatistics(grok.View):
+
+    grok.context(IPloneSiteRoot)
+    grok.name('show-statistics')
+
+    def render(self):
+        URL = 'http://birt.osha.europa.eu/birt-viewer/frameset?__report=linkstats/linkreportdirect.rptdesign&State=orange%20%20%20%20&ContentType=Document&Category=.%2A&Path=/&Subsite=main'
+        pm = getToolByName(self.context, 'portal_membership')
+        if pm.isAnonymousUser():
+            raise Unauthorized, 'must be logged in to view statistics'
+        member = pm.getAuthenticatedMember()
+        
+        #parsedurl = urlparse.urlparse(URL)
+        #parsedquery = urlparse.parse_qs(parsedurl.query)
+        #parsedquery['member_id'] = member.id
+        #url = urlparse.urlunparse(parsedurl[:4] + (urllib.urlencode(parsedquery),) + parsedurl[5:])
+        url = URL + '&member_id=%s' % member.id
+
+        page = urllib2.urlopen(url)
+        return page.read()
+        
+        

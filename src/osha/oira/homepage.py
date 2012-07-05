b99cdf19ebf049856dacef46d26fedbe2f297c95
JC Brand <jc@opkode.com>
2012-07-05 10:47:20 +0200

    Customize the Homepage edit form to add better buttons and actions.

--- src/osha/oira/homepage.py
+++ src/osha/oira/homepage.py
@@ -1,10 +1,16 @@
 from five import grok
 from zope import schema
 from zope.interface import implements
+from zope.event import notify
+
+from z3c.form import button
+from plone.z3cform import layout
 
 from plone.directives import form
 from plone.directives import dexterity
+from plone.dexterity.browser import edit
 from plone.app.dexterity.behaviors.metadata import IBasic
+from plone.dexterity.events import EditFinishedEvent
 
 from euphorie.content import MessageFactory as _
 
@@ -29,9 +35,33 @@ class View(grok.View):
     grok.context(IHomePage)
     grok.require("zope2.View")
     grok.layer(IOSHAContentSkinLayer)
-    grok.name("oira-homepage")
     grok.template("custom_homepage")
+    grok.name("nuplone-view")
 
     def render_body(self):
         return self.context.description
 
+
+class EditForm(edit.DefaultEditForm):
+    """ """
+    buttons = edit.DefaultEditForm.buttons
+    preview = button.Buttons(button.Button('preview', title=u'Save and preview'))
+    buttons = preview + buttons
+    buttons['cancel'].title = u'Clear unsaved changes'
+    handlers = edit.DefaultEditForm.handlers
+
+    def nextURL(self):
+        return 'edit'
+
+    @button.handler(buttons['preview'])
+    def handle_preview(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        self.applyChanges(data)
+        self.request.response.redirect('nuplone-view')
+        notify(EditFinishedEvent(self.context))
+    
+
+EditView = layout.wrap_form(EditForm)

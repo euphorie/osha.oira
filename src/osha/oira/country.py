491526dda58c883f5a45bdcefec9cf3cfc79c2f6
JC Brand <jc@opkode.com>
2012-09-28 14:26:39 +0200

    Remove country customizations (in euphorie)

--- src/osha/oira/country.py
+++ src/osha/oira/country.py
@@ -1,29 +1,9 @@
 from five import grok
-from euphorie.content.country import View as ContentView
 from euphorie.client.country import View as EuphorieView
-from euphorie.content.sector import ISector
 from osha.oira.interfaces import IOSHAClientSkinLayer
-from osha.oira.interfaces import IOSHAContentSkinLayer
 
 grok.templatedir("templates")
 
 class View(EuphorieView):
     grok.layer(IOSHAClientSkinLayer)
     grok.template("sessions")
-
-class View(ContentView):
-    grok.layer(IOSHAContentSkinLayer)
-
-    def update(self):
-        super(View, self).update()
-        names=self.request.locale.displayNames.territories
-        self.title=names.get(self.context.id.upper(), self.context.title)
-        self.sectors=[dict(id=sector.id,
-                           title=sector.title,
-                           url=sector.absolute_url())
-                      for sector in self.context.values()
-                      if ISector.providedBy(sector)]
-        try:
-            self.sectors.sort(key=lambda s: s["title"].lower())
-        except UnicodeDecodeError:
-            self.sectors.sort(key=lambda s: s["title"].lower().decode('utf-8'))

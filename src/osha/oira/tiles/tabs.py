8456b04b3cd985e066edf3590f969b5e3929b9f6
JC Brand <jc@opkode.com>
2012-11-30 14:29:41 +0200

    6071: Hide help tab

---
+++ src/osha/oira/tiles/tabs.py
@@ -0,0 +1,11 @@
+from euphorie.deployment.tiles.tabs import SiteRootTabsTile
+
+class OiRASiteRootTabsTile(SiteRootTabsTile):
+
+    def update(self):
+        super(OiRASiteRootTabsTile, self).update()
+        for r in self.tabs:
+            if r.get('id') == 'help':
+                self.tabs.remove(r)
+
+                

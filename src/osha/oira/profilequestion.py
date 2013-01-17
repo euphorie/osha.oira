940260f8928addb4b26aa261a86cf0d5460164ca
Wichert Akkerman <wichert@wiggy.net>
2013-01-17 16:56:45 +0100

    Add extra fields to profile question

--- src/osha/oira/profilequestion.py
+++ src/osha/oira/profilequestion.py
@@ -1,12 +1,71 @@
 from five import grok
-from euphorie.content import profilequestion
+from zope import schema
+from zope.component import adapter
+from zope.interface import implementer
+from plone.directives import dexterity
+from plone.directives import form
+from euphorie.content.profilequestion import IProfileQuestion
+from euphorie.content.profilequestion import View as BaseView
 from .interfaces import IOSHAContentSkinLayer
+from . import _
 
 grok.templatedir("templates")
 
 
-class View(profilequestion.View):
+class IOSHAProfileQuestion(IProfileQuestion):
+    question = schema.TextLine(
+            title=_('label_profilequestion_question', default=u'Question'),
+            description=_(u'This question must ask users if this profile '
+                          u'applies to them.'),
+            required=True)
+
+    label_single_occurances = schema.TextLine(
+            title=_(u'Single occurance prompt'),
+            description=_(u'This must ask to user for the name of the '
+                          u'relevant location.'),
+            required=True)
+    form.widget(label_single_occurances='euphorie.content.profilequestion.TextSpan7')
+    form.order_after(label_single_occurances='question')
+
+    label_multiple_occurances = schema.TextLine(
+            title=_(u'Multiple occurance prompt'),
+            description=_(u'This must ask to user for the names of all '
+                          u'relevant locations.'),
+            required=True)
+    form.widget(label_multiple_occurances='euphorie.content.profilequestion.TextSpan7')
+    form.order_after(label_multiple_occurances='label_single_occurances')
+
+
+@adapter(IProfileQuestion)
+@implementer(IOSHAProfileQuestion)
+def context_proxy(content):
+    """Trivial adapter to present all standard profile questions implement
+    IOSHAProfileQuestion. This allows us to use IOSHAProfileQuestion without
+    persisting it or a custom content class in the database so we keep a clean
+    migration path.
+    """
+    return content
+
+
+class View(BaseView):
     """ Override so that we can use our own template
     """
     grok.template("profilequestion_view")
     grok.layer(IOSHAContentSkinLayer)
+
+
+class AddForm(dexterity.AddForm):
+    grok.context(IProfileQuestion)
+    grok.name('euphorie.profilequestion')
+    grok.require('euphorie.content.AddNewRIEContent')
+    grok.layer(IOSHAContentSkinLayer)
+
+    schema = IOSHAProfileQuestion
+
+
+class EditForm(dexterity.EditForm):
+    grok.context(IProfileQuestion)
+    grok.require('cmf.ModifyPortalContent')
+    grok.layer(IOSHAContentSkinLayer)
+
+    schema = IOSHAProfileQuestion

6cfb21b9e416f9a2f4ae1fda8608c938c8618746
Wichert Akkerman <wichert@wiggy.net>
2013-01-20 22:01:24 +0100

    Copy update.pt from euphorie for modification.

--- src/osha/oira/profile.py
+++ src/osha/oira/profile.py
@@ -6,6 +6,23 @@ from . import _
 grok.templatedir("templates")
 
 
+def _questions(context):
+    return [{'id': child.id,
+             'title': child.title,
+             'question': child.question or child.title,
+             'label_multiple_present': getattr(child,
+                 'label_multiple_present',
+                 _(u'Does this happen in multiple places?')),
+             'label_single_occurance': getattr(child,
+                 'label_single_occurance',
+                 _(u'Enter the name of the location')),
+             'label_multiple_occurances': getattr(child,
+                 'label_multiple_occurances',
+                 _(u'Enter the names of each location')),
+             }
+            for child in context.ProfileQuestions()]
+
+
 class OSHAProfile(profile.Profile):
     """ Override the original profile to provide our own template.
     """
@@ -14,17 +31,14 @@ class OSHAProfile(profile.Profile):
     grok.name("profile")
 
     def ProfileQuestions(self):
-        return [{'id': child.id,
-                 'title': child.title,
-                 'question': child.question or child.title,
-                 'label_multiple_present': getattr(child,
-                     'label_multiple_present',
-                     _(u'Does this happen in multiple places?')),
-                 'label_single_occurance': getattr(child,
-                     'label_single_occurance',
-                     _(u'Enter the name of the location')),
-                 'label_multiple_occurances': getattr(child,
-                     'label_multiple_occurances',
-                     _(u'Enter the names of each location')),
-                 }
-                for child in self.context.ProfileQuestions()]
+        return _questions(self.context)
+
+
+class OSHAUpdate(profile.Update):
+    """ Override the original profile to provide our own template.
+    """
+    grok.layer(interfaces.IOSHAClientSkinLayer)
+    grok.template('update')
+
+    def ProfileQuestions(self):
+        return _questions(self.context)

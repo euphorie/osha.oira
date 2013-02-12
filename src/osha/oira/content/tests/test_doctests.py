81b9a9c06934f15ef6bced44472d4c8b6f2a96be
Wichert Akkerman <wichert@wiggy.net>
2013-02-12 15:02:01 +0100

    Move more tests out to subpackages.

---
+++ src/osha/oira/content/tests/test_doctests.py
@@ -0,0 +1,25 @@
+import doctest
+import glob
+import os.path
+import unittest
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+from osha.oira.tests.base import OiRAFunctionalTestCase
+
+
+def test_suite():
+    location = os.path.dirname(__file__) or "."
+    doctests = ["stories/" + os.path.basename(test)
+                for test in glob.glob(os.path.join(location, "stories", "*.txt"))]
+
+    options = doctest.REPORT_ONLY_FIRST_FAILURE | \
+              doctest.ELLIPSIS | \
+              doctest.NORMALIZE_WHITESPACE
+
+    suites = [FunctionalDocFileSuite(test,
+                                    optionflags=options,
+                                    test_class=OiRAFunctionalTestCase,
+                                    module_relative=True,
+                                    package="osha.oira.content.tests",
+                                    encoding="utf-8")
+              for test in doctests]
+    return unittest.TestSuite(suites)

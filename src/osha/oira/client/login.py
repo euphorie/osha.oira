d519613e79ea96827af07006ca52fa8f8cdd9214
Wichert Akkerman <wichert@wiggy.net>
2013-02-13 10:13:17 +0100

    Fix logging of statistics login.

--- src/osha/oira/client/login.py
+++ src/osha/oira/client/login.py
@@ -1,11 +1,20 @@
 from five import grok
-from euphorie.client import login as register
+from euphorie.client.login import Login as BaseLogin
+from euphorie.client.login import Register as BaseRegister
 from .interfaces import IOSHAClientSkinLayer
+from .model import LoginStatistics
 
 grok.templatedir("templates")
 
 
-class Register(register.Register):
-    grok.require("zope2.View")
+class Login(BaseLogin):
+    grok.layer(IOSHAClientSkinLayer)
+
+    def login(self, account, remember):
+        account.logins.append(LoginStatistics(account=account))
+        return super(Login, self).login(account, remember)
+
+
+class Register(BaseRegister):
     grok.layer(IOSHAClientSkinLayer)
     grok.template("register")

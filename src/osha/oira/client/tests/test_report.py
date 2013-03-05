b85bb87d59efc60aafcac3f36dacc9243d82196f
Wichert Akkerman <wichert@wiggy.net>
2013-03-05 08:15:36 +0100

    PEP8

--- src/osha/oira/client/tests/test_report.py
+++ src/osha/oira/client/tests/test_report.py
@@ -1,24 +1,23 @@
 # coding=utf-8
 
 from Products.Five.testbrowser import Browser
-from euphorie.client.tests.utils import addSurvey
-from euphorie.client.tests.utils import registerUserInClient
-
 from osha.oira.tests.base import OiRAFunctionalTestCase
 
-class EuphorieReportTests(OiRAFunctionalTestCase):
 
+class EuphorieReportTests(OiRAFunctionalTestCase):
     def testUnicodeReportFilename(self):
         from euphorie.content.tests.utils import BASIC_SURVEY
+        from euphorie.client.tests.utils import addSurvey
+        from euphorie.client.tests.utils import registerUserInClient
         # Test for http://code.simplon.biz/tracker/euphorie/ticket/156
         self.loginAsPortalOwner()
         addSurvey(self.portal, BASIC_SURVEY)
-        browser=Browser()
-        survey_url=self.portal.client.nl["ict"]["software-development"].absolute_url()
+        browser = Browser()
+        survey_url = self.portal.client.nl["ict"]["software-development"].absolute_url()
         browser.open(survey_url)
         registerUserInClient(browser)
         # Create a new survey session
-        browser.getControl(name="title:utf8:ustring").value=u"Sessiøn".encode("utf-8")
+        browser.getControl(name="title:utf8:ustring").value = u"Sessiøn".encode("utf-8")
         browser.getControl(name="next", index=1).click()
         # Start the survey
         browser.getForm().submit()
@@ -26,54 +25,55 @@ class EuphorieReportTests(OiRAFunctionalTestCase):
         # Force creation of the company data
         browser.open("%s/report/company" % survey_url)
         # Download the report
-        browser.handleErrors=False
+        browser.handleErrors = False
         browser.open("%s/report/download" % survey_url)
         self.assertEqual(browser.headers.type, "application/rtf")
         self.assertEqual(browser.headers.get("Content-Disposition"),
                 'attachment; filename="Action plan Sessi\xc3\xb8n.rtf"')
 
-
     def testInvalidDateDoesNotBreakRendering(self):
         import datetime
-        from euphorie.content.tests.utils import BASIC_SURVEY
         from z3c.saconfig import Session
         from euphorie.client import model
-        
+        from euphorie.content.tests.utils import BASIC_SURVEY
+        from euphorie.client.tests.utils import addSurvey
+        from euphorie.client.tests.utils import registerUserInClient
         # Test for http://code.simplon.biz/tracker/tno-euphorie/ticket/150
         self.loginAsPortalOwner()
         addSurvey(self.portal, BASIC_SURVEY)
-        browser=Browser()
-        survey_url=self.portal.client.nl["ict"]["software-development"].absolute_url()
+        browser = Browser()
+        survey_url = self.portal.client.nl["ict"]["software-development"].absolute_url()
         browser.open(survey_url)
         registerUserInClient(browser)
         # Create a new survey session
-        browser.getControl(name="title:utf8:ustring").value=u"Sessiøn".encode("utf-8")
+        browser.getControl(name="title:utf8:ustring").value = u"Sessiøn".encode("utf-8")
         browser.getControl(name="next", index=1).click()
         # Start the survey
         browser.getForm().submit()
         browser.getLink("Start Risk Identification").click()
         # Update the risk
-        risk=Session.query(model.Risk).first()
-        risk.identification="no"
+        risk = Session.query(model.Risk).first()
+        risk.identification = "no"
         risk.action_plans.append(model.ActionPlan(
             action_plan=u"Do something awesome",
-            planning_start=datetime.date(1,2,3)))
+            planning_start=datetime.date(1, 2, 3)))
         # Render the report
-        browser.handleErrors=False
+        browser.handleErrors = False
         browser.open("http://nohost/plone/client/nl/ict/software-development/report/view")
         # No errors = success
 
-
     def testCountryDefaultsToCurrentCountry(self):
         from euphorie.content.tests.utils import BASIC_SURVEY
+        from euphorie.client.tests.utils import addSurvey
+        from euphorie.client.tests.utils import registerUserInClient
         self.loginAsPortalOwner()
         addSurvey(self.portal, BASIC_SURVEY)
-        browser=Browser()
-        survey_url=self.portal.client.nl["ict"]["software-development"].absolute_url()
+        browser = Browser()
+        survey_url = self.portal.client.nl["ict"]["software-development"].absolute_url()
         browser.open(survey_url)
         registerUserInClient(browser)
         # Create a new survey session
-        browser.getControl(name="title:utf8:ustring").value=u"Sessiøn".encode("utf-8")
+        browser.getControl(name="title:utf8:ustring").value = u"Sessiøn".encode("utf-8")
         browser.getControl(name="next", index=1).click()
         # Start the survey
         browser.getForm().submit()
@@ -82,28 +82,29 @@ class EuphorieReportTests(OiRAFunctionalTestCase):
         browser.open("%s/report/company" % survey_url)
         self.assertEqual(browser.getControl(name="form.widgets.country").value, ["nl"])
 
-
     def testCompanySettingsRoundTrip(self):
         from euphorie.content.tests.utils import BASIC_SURVEY
+        from euphorie.client.tests.utils import addSurvey
+        from euphorie.client.tests.utils import registerUserInClient
         self.loginAsPortalOwner()
         addSurvey(self.portal, BASIC_SURVEY)
-        browser=Browser()
-        survey_url=self.portal.client.nl["ict"]["software-development"].absolute_url()
+        browser = Browser()
+        survey_url = self.portal.client.nl["ict"]["software-development"].absolute_url()
         browser.open(survey_url)
         registerUserInClient(browser)
         # Create a new survey session
-        browser.getControl(name="title:utf8:ustring").value=u"Sessiøn".encode("utf-8")
+        browser.getControl(name="title:utf8:ustring").value = u"Sessiøn".encode("utf-8")
         browser.getControl(name="next", index=1).click()
         # Start the survey
         browser.getForm().submit()
         browser.getLink("Start Risk Identification").click()
         # Enter some company data
         browser.open("%s/report/company" % survey_url)
-        browser.getControl(name="form.widgets.country").value=["be"]
-        browser.getControl(name="form.widgets.employees").value=["50-249"]
-        browser.getControl(name="form.widgets.conductor").value=["staff"]
-        browser.getControl(name="form.widgets.referer").value=["trade-union"]
-        browser.getControl(name="form.widgets.workers_participated").value=['True']
+        browser.getControl(name="form.widgets.country").value = ["be"]
+        browser.getControl(name="form.widgets.employees").value = ["50-249"]
+        browser.getControl(name="form.widgets.conductor").value = ["staff"]
+        browser.getControl(name="form.widgets.referer").value = ["trade-union"]
+        browser.getControl(name="form.widgets.workers_participated").value = ['True']
         browser.getControl(name="form.buttons.next").click()
         # Make sure all fields validated
         self.assertEqual(browser.url, "%s/report/view" % survey_url)
@@ -114,4 +115,3 @@ class EuphorieReportTests(OiRAFunctionalTestCase):
         self.assertEqual(browser.getControl(name="form.widgets.conductor").value, ["staff"])
         self.assertEqual(browser.getControl(name="form.widgets.referer").value, ["trade-union"])
         self.assertEqual(browser.getControl(name="form.widgets.workers_participated").value, ["True"])
-

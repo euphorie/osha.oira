4112a8af95f0495b09e8f7c31acd4b21806e3fec
JC Brand <jc@opkode.com>
2013-03-13 16:22:14 +0200

    Override the custom sitemenu to add statistics action

---
+++ src/osha/oira/content/sitemenu.py
@@ -0,0 +1,40 @@
+from Acquisition import aq_inner
+from five import grok
+from osha.oira import _
+from osha.oira.interfaces import IOSHAContentSkinLayer
+from euphorie.deployment.browser.sitemenu import EuphorieSitemenu
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+
+class Sitemenu(EuphorieSitemenu):
+    grok.layer(IOSHAContentSkinLayer)
+
+    def actions(self):
+        """ See plonetheme.nuplone.skin.sitemenu.py
+            Add extra 'statistics' action.
+        """
+        menu = super(Sitemenu, self).actions()
+        children = menu["children"]
+        submenu = self.statistics()
+        if submenu:
+            children.append(submenu)
+        if children:
+            return menu
+        else:
+            return None
+
+    def statistics(self):
+        context = aq_inner(self.context)
+        request = self.request
+        try:
+            # We do a permission check by trying to render the view
+            getMultiAdapter((context, request), name="show-statistics")
+        except ComponentLookupError:
+            return
+        menu = {"title": _("menu_admin", default=u"Admin")}
+        menu["children"] = [{
+            "title": _("menu_statistics", default=u"Statistics"),
+            "url": "%s/@@show-statistics" % context.absolute_url()
+        }]
+        return menu

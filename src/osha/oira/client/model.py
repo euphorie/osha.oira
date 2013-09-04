0c52056d0d175c1ca58c44f473dedfbdb3405c85
JC Brand <jc@opkode.com>
2013-09-04 14:31:32 +0200

    Import model instead of lots of individual imports

--- src/osha/oira/client/model.py
+++ src/osha/oira/client/model.py
@@ -3,27 +3,19 @@ from sqlalchemy import sql
 from sqlalchemy import types
 from sqlalchemy import orm
 from sqlalchemy.ext.declarative import instrument_declarative
-from euphorie.client.model import metadata
-from euphorie.client.model import Account
-from euphorie.client.model import BaseObject
-from euphorie.client.model import SurveyTreeItem
-from euphorie.client.model import Risk
-from euphorie.client.model import Company
-from euphorie.client.model import SKIPPED_PARENTS
-from euphorie.client.model import MODULE_WITH_RISK_OR_TOP5_FILTER
-from euphorie.client.model import RISK_PRESENT_OR_TOP5_FILTER
+from euphorie.client import model
 
 
-class LoginStatistics(BaseObject):
+class LoginStatistics(model.BaseObject):
     """Data table to store login information for users.
     """
     __tablename__ = "statistics_login"
     id = schema.Column(types.Integer(), primary_key=True, autoincrement=True)
     account_id = schema.Column(types.Integer(),
-            schema.ForeignKey(Account.id,
+            schema.ForeignKey(model.Account.id,
                 onupdate="CASCADE", ondelete="CASCADE"),
             nullable=False, index=True)
-    account = orm.relation(Account,
+    account = orm.relation(model.Account,
             backref=orm.backref("logins",
                                 cascade="all, delete, delete-orphan"))
     time = schema.Column(types.DateTime(timezone=False),
@@ -34,52 +26,44 @@ class LoginStatistics(BaseObject):
 _instrumented = False
 if not _instrumented:
     for cls in [LoginStatistics]:
-        instrument_declarative(cls, metadata._decl_registry, metadata)
+        instrument_declarative(cls, model.metadata._decl_registry, model.metadata)
     _instrumented = True
 
-
-# Pyflakes
-Company = Company
-SKIPPED_PARENTS = SKIPPED_PARENTS
-MODULE_WITH_RISK_OR_TOP5_FILTER = MODULE_WITH_RISK_OR_TOP5_FILTER
-RISK_PRESENT_OR_TOP5_FILTER = RISK_PRESENT_OR_TOP5_FILTER
-# Pyflakes
-
-node = orm.aliased(SurveyTreeItem)
+node = orm.aliased(model.SurveyTreeItem)
 
 UNANSWERED_RISKS_FILTER = \
-        sql.and_(SurveyTreeItem.type == "risk",
-                sql.exists(sql.select([Risk.sql_risk_id]).where(sql.and_(
-                    Risk.sql_risk_id == SurveyTreeItem.id,
-                    Risk.identification == None,
+        sql.and_(model.SurveyTreeItem.type == "risk",
+                sql.exists(sql.select([model.Risk.sql_risk_id]).where(sql.and_(
+                    model.Risk.sql_risk_id == model.SurveyTreeItem.id,
+                    model.Risk.identification == None,
                     ))))
 
 MODULE_WITH_UNANSWERED_RISKS_FILTER = \
-        sql.and_(SurveyTreeItem.type == "module",
-                SurveyTreeItem.skip_children == False,
+        sql.and_(model.SurveyTreeItem.type == "module",
+                model.SurveyTreeItem.skip_children == False,
                 sql.exists(sql.select([node.id]).where(sql.and_(
-                    node.session_id == SurveyTreeItem.session_id,
-                    node.id == Risk.sql_risk_id,
+                    node.session_id == model.SurveyTreeItem.session_id,
+                    node.id == model.Risk.sql_risk_id,
                     node.type == "risk",
-                    Risk.identification == None,
-                    node.depth > SurveyTreeItem.depth,
-                    node.path.like(SurveyTreeItem.path + "%")))))
+                    model.Risk.identification == None,
+                    node.depth > model.SurveyTreeItem.depth,
+                    node.path.like(model.SurveyTreeItem.path + "%")))))
 
 MODULE_WITH_RISKS_NOT_PRESENT_FILTER = \
-        sql.and_(SurveyTreeItem.type == "module",
-                SurveyTreeItem.skip_children == False,
+        sql.and_(model.SurveyTreeItem.type == "module",
+                model.SurveyTreeItem.skip_children == False,
                 sql.exists(sql.select([node.id]).where(sql.and_(
-                    node.session_id == SurveyTreeItem.session_id,
-                    node.id == Risk.sql_risk_id,
+                    node.session_id == model.SurveyTreeItem.session_id,
+                    node.id == model.Risk.sql_risk_id,
                     node.type == "risk",
-                    Risk.identification == 'yes',
-                    node.depth > SurveyTreeItem.depth,
-                    node.path.like(SurveyTreeItem.path + "%")))))
+                    model.Risk.identification == 'yes',
+                    node.depth > model.SurveyTreeItem.depth,
+                    node.path.like(model.SurveyTreeItem.path + "%")))))
 
 RISK_NOT_PRESENT_FILTER = \
-        sql.and_(SurveyTreeItem.type == "risk",
-                sql.exists(sql.select([Risk.sql_risk_id]).where(sql.and_(
-                    Risk.sql_risk_id == SurveyTreeItem.id,
-                    Risk.identification == "yes"))))
+        sql.and_(model.SurveyTreeItem.type == "risk",
+                sql.exists(sql.select([model.Risk.sql_risk_id]).where(sql.and_(
+                    model.Risk.sql_risk_id == model.SurveyTreeItem.id,
+                    model.Risk.identification == "yes"))))
 
 del node

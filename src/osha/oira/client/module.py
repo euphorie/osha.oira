4b5d5a285221f96599fac69d68e05bdea2aeb66e
JC Brand <jc@opkode.com>
2013-11-07 14:42:25 +0200

    Skip duplicate profile question node in leftnav. Refs #9015

--- src/osha/oira/client/module.py
+++ src/osha/oira/client/module.py
@@ -1,17 +1,43 @@
+from Acquisition import aq_inner
+from euphorie.client import module
+from euphorie.client.update import redirectOnSurveyUpdate
+from euphorie.client.navigation import FindNextQuestion
+from euphorie.client.navigation import QuestionURL
+from euphorie.content.profilequestion import IProfileQuestion
 from five import grok
-from euphorie.client.module import EvaluationView as ModuleEvaluationView
-from euphorie.client.module import ActionPlanView as ModuleActionPlanView
-from .interfaces import IOSHAEvaluationPhaseSkinLayer
-from .interfaces import IOSHAActionPlanPhaseSkinLayer
+from osha.oira.client import interfaces
 
 grok.templatedir("templates")
 
 
-class EvaluationView(ModuleEvaluationView):
-    grok.layer(IOSHAEvaluationPhaseSkinLayer)
+class IdentificationView(module.IdentificationView):
+    grok.layer(interfaces.IOSHAIdentificationPhaseSkinLayer)
+
+    def update(self):
+        if redirectOnSurveyUpdate(self.request):
+            return
+
+        if self.request.environ["REQUEST_METHOD"] == "POST":
+            return super(IdentificationView, self).update()
+
+        context = aq_inner(self.context)
+        module = self.request.survey.restrictedTraverse(
+                                self.context.zodb_path.split("/"))
+
+        if IProfileQuestion.providedBy(module) and context.depth == 2:
+            next = FindNextQuestion(context, filter=self.question_filter)
+            url = QuestionURL(self.request.survey, next,
+                    phase="identification")
+            return self.request.response.redirect(url)
+        else:
+            return super(IdentificationView, self).update()
+
+
+class EvaluationView(module.EvaluationView):
+    grok.layer(interfaces.IOSHAEvaluationPhaseSkinLayer)
     grok.template("module_evaluation")
 
 
-class ActionPlanView(ModuleActionPlanView):
-    grok.layer(IOSHAActionPlanPhaseSkinLayer)
+class ActionPlanView(module.ActionPlanView):
+    grok.layer(interfaces.IOSHAActionPlanPhaseSkinLayer)
     grok.template("module_actionplan")

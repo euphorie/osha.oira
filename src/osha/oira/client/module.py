ebd5d3326afab0d8a463f29031bf201cdf54c71f
JC Brand <jc@opkode.com>
2013-11-08 16:55:54 +0200

    Also fix duplicate nav node issue for evaluation/actionplan. Refs #9015

--- src/osha/oira/client/module.py
+++ src/osha/oira/client/module.py
@@ -9,16 +9,14 @@ from osha.oira.client import interfaces
 
 grok.templatedir("templates")
 
+class Mixin(object):
 
-class IdentificationView(module.IdentificationView):
-    grok.layer(interfaces.IOSHAIdentificationPhaseSkinLayer)
-
-    def update(self):
+    def _update(self, superclass):
         if redirectOnSurveyUpdate(self.request):
             return
 
         if self.request.environ["REQUEST_METHOD"] == "POST":
-            return super(IdentificationView, self).update()
+            return super(superclass, self).update()
 
         context = aq_inner(self.context)
         module = self.request.survey.restrictedTraverse(
@@ -27,20 +25,38 @@ class IdentificationView(module.IdentificationView):
         if IProfileQuestion.providedBy(module) and context.depth == 2:
             next = FindNextQuestion(context, filter=self.question_filter)
             if next is None:
-                # We ran out of questions, proceed to the evaluation
-                url = "%s/evaluation" % self.request.survey.absolute_url()
+                if self.phase == 'identification':
+                    url = "%s/evaluation" % self.request.survey.absolute_url()
+                elif self.phase == 'evaluation':
+                    url = "%s/actionplan" % \
+                            self.request.survey.absolute_url()
+                elif self.phase == 'actionplan':
+                    url = "%s/report" % self.request.survey.absolute_url()
             else:
-                url = QuestionURL(self.request.survey, next, phase="identification")
+                url = QuestionURL(self.request.survey, next, phase=self.phase)
             return self.request.response.redirect(url)
         else:
-            return super(IdentificationView, self).update()
+            return super(superclass, self).update()
 
 
-class EvaluationView(module.EvaluationView):
+class IdentificationView(module.IdentificationView, Mixin):
+    grok.layer(interfaces.IOSHAIdentificationPhaseSkinLayer)
+
+    def update(self):
+        return self._update(IdentificationView)
+
+
+class EvaluationView(module.EvaluationView, Mixin):
     grok.layer(interfaces.IOSHAEvaluationPhaseSkinLayer)
     grok.template("module_evaluation")
 
+    def update(self):
+        return self._update(EvaluationView)
 
-class ActionPlanView(module.ActionPlanView):
+
+class ActionPlanView(module.ActionPlanView, Mixin):
     grok.layer(interfaces.IOSHAActionPlanPhaseSkinLayer)
     grok.template("module_actionplan")
+
+    def update(self):
+        return self._update(ActionPlanView)

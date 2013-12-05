1da2feb3c39d3c2458b28249d088b020139a9a9b
Lennart Regebro <regebro@gmail.com>
2013-12-05 17:00:30 +0100

    Added an explanatory comment.

--- src/osha/oira/client/surveygroup.py
+++ src/osha/oira/client/surveygroup.py
@@ -8,6 +8,8 @@ class Edit(SchemaEditForm):
 
     def updateWidgets(self):
         result = super(Edit, self).updateWidgets()
+        # Hide the "Obsolete Survey" option, as OiRA doesn't use that feature, and it's
+        # definitely not called a "survey". See https://projects.syslab.com/issues/9133
         widget = self.widgets.get('obsolete')
         widget.mode = form.interfaces.HIDDEN_MODE
         return result

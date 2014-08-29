fe012a3d81a29785392814195d6e716863669426
Wolfgang Thomas <thomas@syslab.com>
2014-08-29 15:52:54 +0200

    In the reset-password form, aloready provide info about the password policy's requirements (feedback from EU-OSHA) #10284

---
+++ src/osha/oira/nuplone/pwreset.py
@@ -0,0 +1,34 @@
+from five import grok
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.CMFCore.utils import getToolByName
+from plonetheme.nuplone.skin import pwreminder
+from ..interfaces import IOSHAContentSkinLayer
+from euphorie.content import MessageFactory as _
+from zope.i18n import translate
+
+
+class PasswordReset(pwreminder.PasswordReset):
+    grok.context(ISiteRoot)
+    grok.name("reset-password")
+    grok.require("zope2.Public")
+    grok.layer(IOSHAContentSkinLayer)
+
+    orig_description = pwreminder.PasswordReset.description
+    extra_description = _(
+        u"Your password must contain at least 5 characters, "
+        u"including at least one capital letter, one number and "
+        u"one special character (e.g. $, # or @).")
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.language = getToolByName(
+            context, 'portal_languages').getPreferredLanguage()
+
+    @property
+    def description(self):
+        # import pdb; pdb.set_trace( )
+        description = u" ".join([
+            translate(self.orig_description, target_language=self.language),
+            translate(self.extra_description, target_language=self.language)])
+        return description

1a19228132ec3d7fd7f70f2d6d098cd808a098ef
JC Brand <jc@opkode.com>
2015-05-28 17:18:48 +0200

    Override the action plan view. refs #11760
    to properly handle dates on measures.

--- src/osha/oira/client/risk.py
+++ src/osha/oira/client/risk.py
@@ -1,12 +1,16 @@
-from five import grok
+from Acquisition import aq_inner
+from euphorie.client import model
 from euphorie.client import risk
+from euphorie.client.navigation import FindNextQuestion
+from euphorie.client.navigation import FindPreviousQuestion
+from euphorie.client.navigation import QuestionURL
+from euphorie.client.navigation import getTreeData
+from euphorie.client.session import SessionManager
 from euphorie.client.update import redirectOnSurveyUpdate
 from euphorie.client.utils import HasText
-from euphorie.client.navigation import getTreeData
-from euphorie.client.navigation import FindPreviousQuestion
-from euphorie.client.navigation import FindNextQuestion
-from euphorie.client.navigation import QuestionURL
-from euphorie.client.session import SessionManager
+from euphorie.content.solution import ISolution
+from five import grok
+from z3c.saconfig import Session
 from .interfaces import IOSHAIdentificationPhaseSkinLayer
 from .interfaces import IOSHAActionPlanPhaseSkinLayer
 
@@ -79,3 +83,88 @@ class OSHAIdentificationView(risk.EvaluationView):
 class OSHAActionPlanView(risk.ActionPlanView):
     grok.layer(IOSHAActionPlanPhaseSkinLayer)
     grok.template("risk_actionplan")
+
+    def update(self):
+        if redirectOnSurveyUpdate(self.request):
+            return
+
+        context = aq_inner(self.context)
+        self.errors = {}
+        if self.request.environ["REQUEST_METHOD"] == "POST":
+            reply = self.request.form
+            session = Session()
+            errors = False
+            reply["action_plans"] = []
+            new_plans = []
+
+            for i in range(0, len(reply['measure'])):
+                measure = dict([p for p in reply['measure'][i].items()
+                                if p[1].strip()])
+                reply['action_plans'].append(measure)
+                start = measure.get('planning_start')
+                end = measure.get('planning_end')
+                if start and end and start > end:
+                    reply['action_plans'][-1]['errors'][
+                        'planning_start_date'] = [
+                            u'Start date is not before end date']
+
+                if len(measure):
+                    new_plans.append(
+                        model.ActionPlan(
+                            action_plan=measure.get("action_plan"),
+                            prevention_plan=measure.get("prevention_plan"),
+                            requirements=measure.get("requirements"),
+                            responsible=measure.get("responsible"),
+                            budget=measure.get("budget"),
+                            planning_start=measure.get('planning_start'),
+                            planning_end=measure.get('planning_start')
+                        )
+                    )
+            if errors:
+                self.data = reply
+            else:
+                context.comment = reply.get("comment")
+                context.priority = reply.get("priority")
+
+                for plan in context.action_plans:
+                    session.delete(plan)
+                context.action_plans.extend(new_plans)
+                SessionManager.session.touch()
+
+                if reply["next"] == "previous":
+                    next = FindPreviousQuestion(
+                        context, filter=self.question_filter)
+                    if next is None:
+                        # We ran out of questions, step back to intro page
+                        url = "%s/evaluation" \
+                              % self.request.survey.absolute_url()
+                        self.request.response.redirect(url)
+                        return
+                else:
+                    next = FindNextQuestion(
+                        context, filter=self.question_filter)
+                    if next is None:
+                        # We ran out of questions, proceed to the report
+                        url = "%s/report" % self.request.survey.absolute_url()
+                        self.request.response.redirect(url)
+                        return
+
+                url = QuestionURL(
+                    self.request.survey, next, phase="actionplan")
+                self.request.response.redirect(url)
+                return
+        else:
+            if len(context.action_plans) == 0:
+                context.action_plans.append(model.ActionPlan())
+            self.data = context
+
+        self.risk = self.request.survey.restrictedTraverse(
+            context.zodb_path.split("/"))
+        self.title = context.parent.title
+        self.tree = getTreeData(
+            self.request, context,
+            filter=self.question_filter, phase="actionplan")
+        self.solutions = [solution for solution in self.risk.values()
+                          if ISolution.providedBy(solution)]
+        super(risk.ActionPlanView, self).update()
+

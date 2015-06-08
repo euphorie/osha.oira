f0d1865d8baa9d028a3ab27a676ab49ec6ca1c62
JC Brand <jc@opkode.com>
2015-06-08 14:10:52 +0200

    Update the action plan view to properly show custom risks. #11760

--- src/osha/oira/client/risk.py
+++ src/osha/oira/client/risk.py
@@ -107,97 +107,99 @@ class OSHAActionPlanView(risk.ActionPlanView):
     grok.layer(IOSHAActionPlanPhaseSkinLayer)
     grok.template("risk_actionplan")
 
+
     def update(self):
         if redirectOnSurveyUpdate(self.request):
             return
-
         context = aq_inner(self.context)
-        self.errors = {}
         if self.request.environ["REQUEST_METHOD"] == "POST":
             reply = self.request.form
             session = Session()
-            errors = False
-            reply["action_plans"] = []
-            new_plans = []
+            context.comment = reply.get("comment")
+            context.priority = reply.get("priority")
 
-            for i in range(0, len(reply['measure'])):
-                measure = dict([p for p in reply['measure'][i].items()
-                                if p[1].strip()])
-                reply['action_plans'].append(measure)
-                start = measure.get('planning_start')
-                end = measure.get('planning_end')
-                if start and end and start > end:
-                    reply['action_plans'][-1]['errors'][
-                        'planning_start_date'] = [
-                            u'Start date is not before end date']
+            for plan in context.action_plans:
+                session.delete(plan)
+            context.action_plans.extend(self.extract_plans_from_request())
+            SessionManager.session.touch()
 
-                if len(measure):
-                    new_plans.append(
-                        model.ActionPlan(
-                            action_plan=measure.get("action_plan"),
-                            prevention_plan=measure.get("prevention_plan"),
-                            requirements=measure.get("requirements"),
-                            responsible=measure.get("responsible"),
-                            budget=measure.get("budget"),
-                            planning_start=measure.get('planning_start'),
-                            planning_end=measure.get('planning_start')
-                        )
-                    )
-            if errors:
-                self.data = reply
+            if reply["next"] == "previous":
+                next = FindPreviousQuestion(
+                    context, filter=self.question_filter)
+                if next is None:
+                    # We ran out of questions, step back to intro page
+                    url = "%s/evaluation" \
+                            % self.request.survey.absolute_url()
+                    return self.request.response.redirect(url)
             else:
-                context.comment = reply.get("comment")
-                context.priority = reply.get("priority")
-
-                for plan in context.action_plans:
-                    session.delete(plan)
-                context.action_plans.extend(new_plans)
-                SessionManager.session.touch()
-
-                if reply["next"] == "previous":
-                    next = FindPreviousQuestion(
-                        context, filter=self.question_filter)
-                    if next is None:
-                        # We ran out of questions, step back to intro page
-                        url = "%s/evaluation" \
-                              % self.request.survey.absolute_url()
-                        self.request.response.redirect(url)
-                        return
-                else:
-                    next = FindNextQuestion(
-                        context, filter=self.question_filter)
-                    if next is None:
-                        # We ran out of questions, proceed to the report
-                        url = "%s/report" % self.request.survey.absolute_url()
-                        self.request.response.redirect(url)
-                        return
-
-                url = QuestionURL(
-                    self.request.survey, next, phase="actionplan")
-                self.request.response.redirect(url)
-                return
+                next = FindNextQuestion(
+                    context, filter=self.question_filter)
+                if next is None:
+                    # We ran out of questions, proceed to the report
+                    url = "%s/report" % self.request.survey.absolute_url()
+                    return self.request.response.redirect(url)
+            url = QuestionURL(
+                self.request.survey, next, phase="actionplan")
+            self.request.response.redirect(url)
+            return
         else:
             if len(context.action_plans) == 0:
                 context.action_plans.append(model.ActionPlan())
             self.data = context
 
-        self.risk = self.request.survey.restrictedTraverse(
-            context.zodb_path.split("/"))
         self.title = context.parent.title
         self.tree = getTreeData(
-            self.request, context,
-            filter=self.question_filter, phase="actionplan")
-        self.solutions = [solution for solution in self.risk.values()
-                          if ISolution.providedBy(solution)]
-        number_images = getattr(self.risk, 'image', None) and 1 or 0
-        if number_images:
-            for i in range(2, 5):
-                number_images += getattr(
-                    self.risk, 'image{0}'.format(i), None) and 1 or 0
+                self.request, context,
+                filter=self.question_filter, phase="actionplan")
+        if self.context.is_custom_risk:
+            self.risk = self.context
+            self.description_intro = u""
+            number_images = 0
+        else:
+            self.risk = self.request.survey.restrictedTraverse(
+                context.zodb_path.split("/"))
+            number_images = getattr(self.risk, 'image', None) and 1 or 0
+            if number_images:
+                for i in range(2, 5):
+                    number_images += getattr(
+                        self.risk, 'image{0}'.format(i), None) and 1 or 0
+            ploneview = getMultiAdapter(
+                (self.context, self.request), name="plone")
+            self.description_intro = ploneview.cropText(
+                StripMarkup(self.risk.description), 200)
+            self.solutions = [solution for solution in self.risk.values()
+                            if ISolution.providedBy(solution)]
+
         self.has_images = number_images > 0
         self.image_class = IMAGE_CLASS[number_images]
-        ploneview = getMultiAdapter(
-            (self.context, self.request), name="plone")
-        self.description_intro = ploneview.cropText(
-            StripMarkup(self.risk.description), 200)
         super(risk.ActionPlanView, self).update()
+
+    def extract_plans_from_request(self):
+        """ Create new ActionPlan objects by parsing the Request.
+        """
+        new_plans = []
+        form = self.request.form
+        form["action_plans"] = []
+        for i in range(0, len(form['measure'])):
+            measure = dict([p for p in form['measure'][i].items()
+                            if p[1].strip()])
+            form['action_plans'].append(measure)
+            start = measure.get('planning_start')
+            end = measure.get('planning_end')
+            if start and end and start > end:
+                form['action_plans'][-1]['errors'][
+                    'planning_start_date'] = [
+                        u'Start date is not before end date']
+            if len(measure):
+                new_plans.append(
+                    model.ActionPlan(
+                        action_plan=measure.get("action_plan"),
+                        prevention_plan=measure.get("prevention_plan"),
+                        requirements=measure.get("requirements"),
+                        responsible=measure.get("responsible"),
+                        budget=measure.get("budget"),
+                        planning_start=measure.get('planning_start'),
+                        planning_end=measure.get('planning_start')
+                    )
+                )
+        return new_plans

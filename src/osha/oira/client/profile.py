730fa3bddd507f19a81b67b603bc55e9a7ee3f6b
JC Brand <jc@opkode.com>
2015-06-15 11:16:35 +0200

    Override and add check to avoid inadvertantly creating locations.
    refs #11760
    
    See: https://github.com/euphorie/Euphorie/commit/40c67d65666877fb1960bce17b14d80004d651a6

--- src/osha/oira/client/profile.py
+++ src/osha/oira/client/profile.py
@@ -1,5 +1,6 @@
 from five import grok
 from euphorie.client import profile
+from euphorie.content.profilequestion import IProfileQuestion
 from .interfaces import IOSHAClientSkinLayer
 from .. import _
 
@@ -32,6 +33,32 @@ class OSHAProfile(profile.Profile):
 
     def ProfileQuestions(self):
         return _questions(self.context)
+
+    def getDesiredProfile(self):
+        """Get the requested profile from the request.
+
+        The profile is returned as a dictionary. The id of the profile
+        questions are used as keys. For optional profile questions the value is
+        a boolean.  For repetable profile questions the value is a list of
+        titles as provided by the user. This format is compatible with
+        :py:func:`extractProfile`.
+
+        :rtype: dictionary with profile answers
+        """
+        profile = {}
+        for (id, answer) in self.request.form.items():
+            question = self.context.get(id)
+            if not IProfileQuestion.providedBy(question):
+                continue
+            if not self.request.get("pq{0}.present".format(id), '') == 'yes':
+                continue
+            if isinstance(answer, list):
+                profile[id] = filter(None, (a.strip() for a in answer))
+                if not self.request.get("pq{0}.multiple".format(id), '') == 'yes':
+                    profile[id] = profile[id][:1]
+            else:
+                profile[id] = answer
+        return profile
 
 
 class OSHAUpdate(profile.Update):

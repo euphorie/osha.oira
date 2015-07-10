2b5b7386f0f1b7d7c64a97b4dc518cf996f9ffa7
Wolfgang Thomas <thomas@syslab.com>
2015-07-10 12:06:15 +0200

    q&d proposal for explicit quitting of a test session

--- src/osha/oira/client/login.py
+++ src/osha/oira/client/login.py
@@ -2,6 +2,7 @@ from five import grok
 from euphorie.client.login import Login as BaseLogin
 from euphorie.client.login import Register as BaseRegister
 from euphorie.client.conditions import TermsAndConditions as BaseTermsAndConditions
+from zope.interface import Interface
 from .interfaces import IOSHAClientSkinLayer
 from .model import LoginStatistics
 
@@ -32,3 +33,10 @@ class TermsAndConditions(BaseTermsAndConditions):
     grok.name("terms-and-conditions")
     grok.layer(IOSHAClientSkinLayer)
     grok.template("conditions")
+
+
+class LogoutConfirmation(grok.View):
+    grok.context(Interface)
+    grok.layer(IOSHAClientSkinLayer)
+    grok.name("logout_confirmation")
+    grok.template("logout_confirmation")

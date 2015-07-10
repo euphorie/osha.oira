aa1d1a785911d66ceac78fc556c17abc79d8154e
Wolfgang Thomas <thomas@syslab.com>
2015-07-10 14:20:32 +0200

    added password reminder form. I deviate from proto here by not placing the password reminder form into a modal. But the technical problems of injecting a modal into a modal make a stand-alone form the easier option for now.

--- src/osha/oira/client/login.py
+++ src/osha/oira/client/login.py
@@ -1,9 +1,13 @@
+from Acquisition import aq_inner
+from Products.statusmessages.interfaces import IStatusMessage
 from five import grok
 from euphorie.client.login import Login as BaseLogin
 from euphorie.client.login import Register as BaseRegister
+from euphorie.client.login import Reminder as BaseReminder
 from euphorie.client.conditions import TermsAndConditions as BaseTermsAndConditions
 from .interfaces import IOSHAClientSkinLayer
 from .model import LoginStatistics
+from osha.oira import _
 
 grok.templatedir("templates")
 
@@ -32,3 +36,24 @@ class TermsAndConditions(BaseTermsAndConditions):
     grok.name("terms-and-conditions")
     grok.layer(IOSHAClientSkinLayer)
     grok.template("conditions")
+
+
+class Reminder(BaseReminder):
+    grok.layer(IOSHAClientSkinLayer)
+    grok.name("reminder")
+    grok.template("reminder")
+
+    def update(self):
+        context = aq_inner(self.context)
+        self.back_url = self.request.form.get("came_from")
+        if not self.back_url:
+            self.back_url = context.absolute_url()
+
+        if self.request.environ["REQUEST_METHOD"] == "POST":
+            if self.request.form.get('cancel', ''):
+                self.request.response.redirect(self.back_url)
+            if self._sendReminder():
+                flash = IStatusMessage(self.request).addStatusMessage
+                flash(_(u"An email with a password reminder has been "
+                        u"sent to your address."), "notice")
+                self.request.response.redirect(self.back_url)

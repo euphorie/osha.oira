b9ed237afb0423717add4c7a6835893461c973aa
JC Brand <jc@opkode.com>
2015-07-14 15:57:12 +0200

    Implement part of the requirements for an updated status page.
    - Updated SQL query to also fetch risks with and without measures.
    - Updated the template to look like proto
    
    refs #10969

--- src/osha/oira/client/survey.py
+++ src/osha/oira/client/survey.py
@@ -1,13 +1,17 @@
 from Acquisition import aq_inner
 from Acquisition import aq_parent
+from decimal import Decimal
+from euphorie.client import model
 from euphorie.client import survey, report
-from euphorie.client.navigation import getTreeData
-from euphorie.client.update import redirectOnSurveyUpdate
 from euphorie.client.navigation import FindFirstQuestion
 from euphorie.client.navigation import QuestionURL
+from euphorie.client.navigation import getTreeData
+from euphorie.client.session import SessionManager
+from euphorie.client.update import redirectOnSurveyUpdate
 from five import grok
-from zope.component import getMultiAdapter
 from osha.oira.client import interfaces
+from z3c.saconfig import Session
+from zope.component import getMultiAdapter
 
 grok.templatedir("templates")
 
@@ -133,3 +137,73 @@ class OSHAStatus(survey.Status):
     grok.layer(interfaces.IOSHAClientSkinLayer)
     grok.template("status")
     grok.name("status")
+
+    query = """SELECT SUBSTRING(path FROM 1 FOR 3) AS module,
+                    CASE WHEN EXISTS(
+                                SELECT * FROM tree AS parent_node
+                                WHERE tree.session_id=parent_node.session_id AND
+                                        tree.depth>parent_node.depth AND
+                                        tree.path LIKE parent_node.path || '%%' AND
+                                        parent_node.skip_children)
+                                THEN 'ignore'
+                        WHEN postponed
+                                THEN 'postponed'
+                        WHEN type='module' AND skip_children='f'
+                                THEN 'ignore'
+                        WHEN type='module' AND postponed IS NOT NULL
+                                THEN 'ok'
+                        WHEN type='risk' AND (SELECT identification
+                                                FROM risk
+                                                WHERE risk.id=tree.id) IN ('yes', 'n/a')
+                                THEN 'ok'
+                        WHEN type='risk' AND (SELECT identification FROM risk
+                                                WHERE risk.id=tree.id AND (SELECT COUNT(id) FROM action_plan WHERE risk.id=action_plan.risk_id)>0
+                                             )='no'
+                                THEN 'risk_with_measures'
+                        WHEN type='risk' AND (SELECT identification FROM risk
+                                                WHERE risk.id=tree.id AND (SELECT COUNT(id) FROM action_plan WHERE risk.id=action_plan.risk_id)=0
+                                             )='no'
+                                THEN 'risk_without_measures'
+                        ELSE 'todo'
+                    END AS status,
+                    COUNT(*) AS count
+            FROM tree
+            WHERE session_id=%(sessionid)d
+            GROUP BY module, status;"""
+
+    def getStatus(self):
+        # Note: Optional modules with a yes-answer are not distinguishable
+        # from non-optional modules, and ignored.
+        session_id = SessionManager.id
+        query = self.query % dict(sessionid=session_id)
+        session = Session()
+        result = session.execute(query).fetchall()
+        
+        total_ok = 0
+        total = 0
+
+        modules = {}
+        base_url = "%s/identification" % self.request.survey.absolute_url()
+        for row in result:
+            module = modules.setdefault(row.module, dict())
+            if "url" not in module:
+                module["url"] = "%s/%s" % (base_url, int(row.module))
+            module["path"] = row.module
+            if row.status != "ignore":
+                module["total"] = module.get("total", 0) + row.count
+                if row.status == 'ok':
+                    total_ok += row.count
+                total += row.count
+
+            module[row.status] = {'count': row.count}
+
+        titles = dict(session.query(model.Module.path, model.Module.title)
+                .filter(model.Module.session_id == session_id)
+                .filter(model.Module.path.in_(modules.keys())))
+
+        for module in modules.values():
+            module["title"] = titles[module["path"]]
+
+        self.percentage_ok = int(total_ok/Decimal(total)*100)
+        self.status = modules.values()
+        self.status.sort(key=lambda m: m["path"])

d3c5cf2c5039269852f5bdfc2ac29b4ab84c6473
Wolfgang Thomas <thomas@syslab.com>
2015-08-04 18:35:18 +0200

    add more manual translations. mainly to move translations from Euphorie to oira

--- src/osha/oira/manual_translations.py
+++ src/osha/oira/manual_translations.py
@@ -214,6 +214,27 @@ _("expl_error", default="We're sorry, but an unforseen error has occured.")
 _("label_remove_measure", default="Delete this measure")
 _("Add Solution", default=u"Add Measure")
 
+# Copied from euphorie/client/templates/risk_actionplan.pt
+# Currently, those modal dialogs are not implemented in oiranew
+_(u"Are you sure you want to delete this measure? This action can not be reverted.",
+    default=u"Are you sure you want to delete this measure? This action can not be reverted.")
+_(u"The current text in the fields 'Action plan', 'Prevention plan' and 'Requirements' of this measure will be overwritten. This action cannot be reverted. Are you sure you want to continue?",
+    default=u"The current text in the fields 'Action plan', 'Prevention plan' and 'Requirements' of this measure will be overwritten. This action cannot be reverted. Are you sure you want to continue?")
+_(u"Standard solutions")
+
+# More copied translations from euphorie
+_(u"help_evaluation_optional", default=u"This option allows users to skip the evaluation phase.")
+_(u"label_evaluation_phase", default=u"Evaluation phase")
+_(u"Description", default=u"Description")
+_(u"evalmethod_fixed", default=u"Skip evaluation")
+_(u"label_fixed_priority", default=u"priority")
+_(u"message_lock_warn", default=u"Please be aware that you have %s more login attempts before your account will be locked.")
+
+_(u"What is OiRA (Online Interactive Risk Assessment)?", default=u"What is OiRA (Online Interactive Risk Assessment)?")
+_(u"OiRA stands for Online Interactive Risk Assessment. It is a web-based tool that allows you to perform a health and safety risk assessment of your workplace.",
+    default=u"OiRA stands for Online Interactive Risk Assessment. It is a web-based tool that allows you to perform a health and safety risk assessment of your workplace.")
+
+
 # Validation error messages
 _("error_validation_date", default=u"This value must be a valid date.")
 _("error_validation_email", default=u"This value must be a valid email address.")

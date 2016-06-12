ecbea6760d99a01630221f9e43b7b518ae4ab668
Wolfgang Thomas <thomas@syslab.com>
2016-06-12 20:10:22 +0200

    transfer risk_identification to Euphorie

--- src/osha/oira/client/risk.py
+++ src/osha/oira/client/risk.py
@@ -1,4 +1,3 @@
-# coding=utf-8
 from Acquisition import aq_inner
 from Products.statusmessages.interfaces import IStatusMessage
 from euphorie.client import model
@@ -9,7 +8,6 @@ from euphorie.client.navigation import QuestionURL
 from euphorie.client.navigation import getTreeData
 from euphorie.client.session import SessionManager
 from euphorie.client.update import redirectOnSurveyUpdate
-from euphorie.client.utils import HasText
 from euphorie.content.solution import ISolution
 from euphorie.content.utils import StripMarkup
 from five import grok
@@ -17,7 +15,6 @@ from osha.oira import _
 from z3c.saconfig import Session
 from zope.component import getMultiAdapter
 from zope.i18n import translate
-from .interfaces import IOSHAIdentificationPhaseSkinLayer
 from .interfaces import IOSHAActionPlanPhaseSkinLayer
 
 grok.templatedir("templates")
@@ -31,113 +28,6 @@ IMAGE_CLASS = {
 }
 
 DESCRIPTION_CROP_LENGTH = 200
-
-
-class OSHAIdentificationView(risk.EvaluationView):
-    """ This view is a combination of the Euphorie Identification and Evauation
-        views.
-        The risk is both identified and evaluated in this step.
-    """
-    grok.layer(IOSHAIdentificationPhaseSkinLayer)
-    grok.template("risk_identification")
-    question_filter = None
-
-    def update(self):
-        if redirectOnSurveyUpdate(self.request):
-            return
-
-        self.risk = self.request.survey.restrictedTraverse(
-            self.context.zodb_path.split("/"))
-
-        if self.request.environ["REQUEST_METHOD"] == "POST":
-            reply = self.request.form
-            answer = reply.get("answer")
-            self.context.comment = reply.get("comment")
-            self.context.postponed = (answer == "postponed")
-            if self.context.postponed:
-                self.context.identification = None
-            else:
-                self.context.identification = answer
-                if self.risk.type in ('top5', 'policy'):
-                    self.context.priority = 'high'
-                elif self.risk.evaluation_method == 'calculated':
-                    self.calculatePriority(self.risk, reply)
-                elif self.risk.evaluation_method == "direct":
-                    self.context.priority = reply.get("priority")
-
-            SessionManager.session.touch()
-
-            if reply["next"] == "previous":
-                next = FindPreviousQuestion(
-                    self.context,
-                    filter=self.question_filter)
-                if next is None:
-                    # We ran out of questions, step back to intro page
-                    url = "%s/identification" % \
-                        self.request.survey.absolute_url()
-                    self.request.response.redirect(url)
-                    return
-            else:
-                next = FindNextQuestion(
-                    self.context,
-                    filter=self.question_filter)
-                if next is None:
-                    # We ran out of questions, proceed to the action plan
-                    url = "%s/actionplan" % self.request.survey.absolute_url()
-                    self.request.response.redirect(url)
-                    return
-
-            url = QuestionURL(self.request.survey, next, phase="identification")
-            self.request.response.redirect(url)
-        else:
-            self.tree = getTreeData(self.request, self.context)
-            self.title = self.context.parent.title
-            self.show_info = self.risk.image or \
-                HasText(self.risk.description) or \
-                HasText(self.risk.legal_reference)
-            number_images = getattr(self.risk, 'image', None) and 1 or 0
-            if number_images:
-                for i in range(2, 5):
-                    number_images += getattr(
-                        self.risk, 'image{0}'.format(i), None) and 1 or 0
-            self.has_images = number_images > 0
-            self.number_images = number_images
-            self.image_class = IMAGE_CLASS[number_images]
-            number_files = 0
-            for i in range(1, 5):
-                number_files += getattr(
-                    self.risk, 'file{0}'.format(i), None) and 1 or 0
-            self.has_files = number_files > 0
-            self.risk_number = self.context.number
-
-            ploneview = getMultiAdapter(
-                (self.context, self.request), name="plone")
-            stripped_description = StripMarkup(self.risk.description)
-            if len(stripped_description) > DESCRIPTION_CROP_LENGTH:
-                self.description_intro = ploneview.cropText(
-                    stripped_description, DESCRIPTION_CROP_LENGTH)
-            else:
-                self.description_intro = ""
-            self.description_probability = _(
-                u"help_default_probability", default=u"Indicate how "
-                "likely occurence of this risk is in a normal situation.")
-            self.description_frequency = _(
-                u"help_default_frequency", default=u"Indicate how often this "
-                u"risk occurs in a normal situation.")
-            self.description_severity = _(
-                u"help_default_severity", default=u"Indicate the "
-                "severity if this risk occurs.")
-            if getattr(self.request.survey, 'enable_custom_evaluation_descriptions', False):
-                if self.request.survey.evaluation_algorithm != 'french':
-                    custom_dp = getattr(
-                        self.request.survey, 'description_probability', '') or ''
-                    self.description_probability = custom_dp.strip() or self.description_probability
-                custom_df = getattr(self.request.survey, 'description_frequency', '') or ''
-                self.description_frequency = custom_df.strip() or self.description_frequency
-                custom_ds = getattr(self.request.survey, 'description_severity', '') or ''
-                self.description_severity = custom_ds.strip() or self.description_severity
-
-            super(risk.EvaluationView, self).update()
 
 
 class OSHAActionPlanView(risk.ActionPlanView):

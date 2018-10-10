d74794fe8345317f6cd3bb2e9d8046e37135435f
Wolfgang Thomas <thomas@syslab.com>
2018-10-10 14:02:39 +0200

    Temporary style hack, until https://github.com/syslabcom/oira.prototype/issues/70 is properly handled

--- src/osha/oira/client/browser/webhelpers.py
+++ src/osha/oira/client/browser/webhelpers.py
@@ -1,5 +1,24 @@
 # coding=utf-8
 from euphorie.client.browser.webhelpers import Appendix
+from euphorie.client.browser.webhelpers import WebHelpers
+from logging import getLogger
+from euphorie.decorators import reify
+
+log = getLogger(__name__)
+
+
+class OSHAWebHelpers(WebHelpers):
+
+    @reify
+    def styles_override(self):
+
+        css = super(OSHAWebHelpers, self).styles_override
+        css += """
+#osc .miller-columns .browser .item .object-name {
+    color: inherit;
+}
+"""
+        return css
 
 
 class OSHAAppendix(Appendix):

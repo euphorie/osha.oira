eb0df44a75a78a3102bc5a5544f613b3679e4e14
Wolfgang Thomas <thomas@syslab.com>
2018-10-22 17:15:39 +0200

    login has been ds-grokked in Euphorie

--- src/osha/oira/client/conditions.py
+++ src/osha/oira/client/conditions.py
@@ -1,19 +1,9 @@
 # coding=utf-8
 from five import grok
-from euphorie.client import login
 from euphorie.client.conditions import TermsAndConditions as BaseTermsAndConditions  # noqa
 from .interfaces import IOSHAClientSkinLayer
-from .model import LoginStatistics
 
 grok.templatedir("templates")
-
-
-class LoginForm(login.LoginForm):
-    grok.layer(IOSHAClientSkinLayer)
-
-    def login(self, account, remember):
-        account.logins.append(LoginStatistics(account=account))
-        return super(LoginForm, self).login(account, remember)
 
 
 class TermsAndConditions(BaseTermsAndConditions):

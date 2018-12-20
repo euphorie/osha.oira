d3d9e7ba86f5cde03b9d1c1225c73fbad0951d75
ale-rt <alessandro.pisa@gmail.com>
2018-12-20 12:24:55 +0100

    Adapt the password reset functionality to work with pasldap

--- src/osha/oira/nuplone/pwreset.py
+++ src/osha/oira/nuplone/pwreset.py
@@ -1,9 +1,15 @@
+# coding=utf-8
+from ..interfaces import IOSHAContentSkinLayer
+from Acquisition import aq_inner
+from euphorie.content import MessageFactory as _
 from five import grok
+from plonetheme.nuplone import MessageFactory as __
+from plonetheme.nuplone.skin import pwreminder
 from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.utils import getToolByName
-from plonetheme.nuplone.skin import pwreminder
-from ..interfaces import IOSHAContentSkinLayer
-from euphorie.content import MessageFactory as _
+from Products.CMFPlone.PasswordResetTool import InvalidRequestError
+from Products.statusmessages.interfaces import IStatusMessage
+from z3c.form.button import buttonAndHandler
 from zope.i18n import translate
 
 
@@ -32,3 +38,53 @@ class PasswordReset(pwreminder.PasswordReset):
             translate(self.orig_description, target_language=self.language),
             translate(self.extra_description, target_language=self.language)])
         return description
+
+    @buttonAndHandler(_("button_change", default="Change"), name="change")
+    def handleChange(self, action):
+        ''' Override the default behavior to call the requestPassword method
+        passing the username and not the id
+        '''
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+
+        flash = IStatusMessage(self.request).addStatusMessage
+        pas = getToolByName(self.context, "acl_users")
+        ppr = getToolByName(self.context, "portal_password_reset")
+        user = pas.getUser(data["login"])
+        if user is None:
+            flash(
+                __(
+                    "user_name_wrong",
+                    u"The login name you have provided does not match the username from the password-reset email. Please check your spelling."  # noqa: E501
+                ), "error"
+            )
+            return
+        # osha Patch: we need to translate the reset request
+        # because we use the username and not the id
+        record = ppr._requests.get(self.randomstring, ())
+        if record and record[0] == user.getUserId():
+            ppr._requests[self.randomstring] = (
+                user.getUserName(),
+                record[1],
+            )
+        try:
+            ppr.resetPassword(
+                # The osha patch is taking the username instead of the id
+                # original code was:
+                # user.getId(), self.randomstring, data["password"]
+                user.getUserName(), self.randomstring, data["password"]
+            )
+        except InvalidRequestError:
+            flash(
+                __(
+                    "user_name_wrong",
+                    u"The login name you have provided does not match the username from the password-reset email. Please check your spelling."  # noqa: E501
+                ), "error"
+            )
+            return
+
+        flash(_("password_reset", u"Your password has been reset."), "success")
+        portal_url = aq_inner(self.context).absolute_url()
+        self.request.response.redirect("%s/@@login" % portal_url)

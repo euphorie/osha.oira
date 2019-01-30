8c41d445dc64a8233a22c6bf352ba07f46d22874
Wolfgang Thomas <thomas@syslab.com>
2019-01-30 15:30:59 +0100

    actually, we need to replicate the special France interface magic here in this package too, because we need to make sure that the France interfaces inherit from the IOSHA ones. Otherwise, we would get the wrong version of the XLS (timeline) report.

--- src/osha/oira/client/survey.py
+++ src/osha/oira/client/survey.py
@@ -14,14 +14,24 @@ class OSHASurveyPublishTraverser(survey.SurveyPublishTraverser):
         "actionplan": interfaces.IOSHAActionPlanPhaseSkinLayer,
         "report": interfaces.IOSHAReportPhaseSkinLayer,
     })
-    survey.SurveyPublishTraverser.countries.update({
-        'it': {
-            'identification': interfaces.IOSHAItalyIdentificationPhaseSkinLayer,
-            'customization': interfaces.IOSHAItalyCustomizationPhaseSkinLayer,
-            'evaluation': interfaces.IOSHAItalyEvaluationPhaseSkinLayer,
-            'actionplan': interfaces.IOSHAItalyActionPlanPhaseSkinLayer,
-            'report': interfaces.IOSHAItalyReportPhaseSkinLayer},
-    })
+    survey.SurveyPublishTraverser.countries.update(
+        {
+            'it': {
+                'identification': interfaces.IOSHAItalyIdentificationPhaseSkinLayer,
+                'customization': interfaces.IOSHAItalyCustomizationPhaseSkinLayer,
+                'evaluation': interfaces.IOSHAItalyEvaluationPhaseSkinLayer,
+                'actionplan': interfaces.IOSHAItalyActionPlanPhaseSkinLayer,
+                'report': interfaces.IOSHAItalyReportPhaseSkinLayer,
+            },
+            'fr': {
+                'identification': interfaces.IOSHAFranceIdentificationPhaseSkinLayer,
+                'customization': interfaces.IOSHAFranceCustomizationPhaseSkinLayer,
+                'evaluation': interfaces.IOSHAFranceEvaluationPhaseSkinLayer,
+                'actionplan': interfaces.IOSHAFranceActionPlanPhaseSkinLayer,
+                'report': interfaces.IOSHAFranceReportPhaseSkinLayer,
+            },
+        }
+    )
 
 
 class OSHAStatusPrint(survey.Status):

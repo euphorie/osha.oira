93fdb26628c39077a3b6eefb9379a34743e4673d
Wolfgang Thomas <thomas@syslab.com>
2019-08-06 15:39:05 +0200

    reify is dead

--- src/osha/oira/client/browser/webhelpers.py
+++ src/osha/oira/client/browser/webhelpers.py
@@ -2,14 +2,14 @@
 from euphorie.client.browser.webhelpers import Appendix
 from euphorie.client.browser.webhelpers import WebHelpers
 from logging import getLogger
-from euphorie.decorators import reify
+from plone.memoize.instance import memoize
 
 log = getLogger(__name__)
 
 
 class OSHAWebHelpers(WebHelpers):
 
-    @reify
+    @memoize
     def styles_override(self):
 
         css = super(OSHAWebHelpers, self).styles_override

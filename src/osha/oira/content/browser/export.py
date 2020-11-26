2d279104b57bd36136ecfc19ec00de350ca6da93
Wolfgang Thomas <thomas@syslab.com>
2020-11-26 18:44:59 +0100

    content: de-grok Survey upload View

--- src/osha/oira/content/browser/export.py
+++ src/osha/oira/content/browser/export.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from euphorie.content.browser import export
+from euphorie.content.browser import upload
+from euphorie.content.profilequestion import IProfileQuestion
 from lxml import etree
 
 
@@ -18,3 +20,23 @@ class OSHAExportSurvey(export.ExportSurvey):
             if value:
                 etree.SubElement(node, field.replace("_", "-")).text = value
         return node
+
+
+class ImporterMixin(object):
+    def ImportProfileQuestion(self, node, survey):
+        profile = upload.SurveyImporter.ImportProfileQuestion(self, node, survey)
+        if IProfileQuestion.providedBy(profile):
+            for field in PQ_FIELDS:
+                setattr(
+                    profile, field, upload.el_unicode(node, field.replace("_", "-"))
+                )
+        return profile
+
+
+class SurveyImporter(ImporterMixin, upload.SurveyImporter):
+    pass
+
+
+class OSHAImportSurvey(upload.ImportSurvey):
+
+    importer_factory = SurveyImporter

2ee956b91fd2ce2df6ea094ebd15a57166696092
Wolfgang Thomas <thomas@syslab.com>
2020-11-29 09:43:32 +0100

    content: de-grok SectorContainer View

---
+++ src/osha/oira/content/browser/sectorcontainer.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from Acquisition import aq_inner
+from euphorie.content.browser.library import get_library
+from euphorie.content.utils import summarizeCountries
+from Products.Five import BrowserView
+
+
+class SectorContainerView(BrowserView):
+    @property
+    def countries(self):
+        return summarizeCountries(aq_inner(self.context), self.request)
+
+    @property
+    def library_info(self):
+        library = get_library(self.context)
+        return [dict(title=lib["title"], url=lib["url"]) for lib in library]

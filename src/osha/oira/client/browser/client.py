c2891ded66646497055c3d2244e2167f7f9ca094
ale-rt <alessandro.pisa@gmail.com>
2022-11-17 16:30:33 +0100

    Implement some suggestions

--- src/osha/oira/client/browser/client.py
+++ src/osha/oira/client/browser/client.py
@@ -7,25 +7,36 @@ from Products.Five import BrowserView
 class MailingListsJson(BrowserView):
     """Mailing lists (countries, in the future also sectors and tools)"""
 
-    def __call__(self):
-        """Json list of mailing list ids/names"""
-        lists = []
-        catalog = api.portal.get_tool(name="portal_catalog")
-        self.request.response.setHeader("Content-type", "application/json")
+    @property
+    def results(self):
+        """
+        List of mailing list UID/names
+
+        The format fits pat-autosuggest.
+        There is a special label for the "all" list.
+        """
         # TODO: require query.
         # q = self.request.get("q", "").strip().lower()
         # if not q:
-        #     return dumps([])
+        #     return []
 
+        catalog = api.portal.get_tool(name="portal_catalog")
         all_users = {"id": "all", "text": "All OiRA users"}
         # if q in all_users["id"] or q in all_users["text"].lower():
-        lists.append(all_users)
+
+        results = [all_users]
 
         # FIXME: SearchableText="de*" doesn't return Germany
-        countries = catalog(portal_type="euphorie.clientcountry")
-        lists.extend(
-            [{"id": country["id"], "text": country["Title"]} for country in countries]
+        brains = catalog(
+            portal_type="euphorie.clientcountry",
+            sort_on="sortable_title",
         )
-        # TODO: add sectors and tools
+        results.extend(
+            [{"id": brain.UID, "text": brain.Title} for brain in brains]
+        )
+        return results
 
-        return dumps(lists)
+    def __call__(self):
+        """Json list of mailing list UID/names"""
+        self.request.response.setHeader("Content-type", "application/json")
+        return dumps(self.results)

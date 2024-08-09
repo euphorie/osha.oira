5ef094db9b75bf3a446d7f6e1df4860af9df06a1
Maurits van Rees <maurits@vanrees.org>
2024-08-09 12:12:24 +0200

    quaive-unpublish: only for surveygroup, only small template needed.

--- src/osha/oira/ploneintranet/quaive_unpublish.py
+++ src/osha/oira/ploneintranet/quaive_unpublish.py
@@ -1,42 +1,11 @@
-from euphorie.client import MessageFactory as _
-from euphorie.content.interfaces import SurveyUnpublishEvent
+from euphorie.content.browser.surveygroup import Unpublish
 from osha.oira.ploneintranet.quaive_mixin import QuaiveEditFormMixin
-from plone import api
-from z3c.form import button
-from z3c.form import form
-from zope.event import notify
-
-import logging
 
 
-logger = logging.getLogger(__name__)
+class UnpublishSurveyGroup(QuaiveEditFormMixin, Unpublish):
+    """Wrapper around Unpublish page designed to be embedded in Quaive
 
-
-class UnpublishSurveyQuaiveForm(QuaiveEditFormMixin, form.Form):
-    """Custom edit form designed to be embedded in Quaive
-
-    This does what euphorie.content.browser.surveygroup.Unpublish does.
-    But here it is called on a Survey instead of a SurveyGroup.
-    And it uses z3c.form instead of a BrowserView.
+    This lets euphorie.content.browser.surveygroup.Unpublish do its work on a
+    POST request.  But it has a template with only the bare minimum that we
+    need from 'euphorie.client.browser.templates.surveygroup_unpublish.pt'.
     """
-
-    def nextURL(self):
-        return f"{self.context.absolute_url()}/@@quaive-edit"
-
-    @button.buttonAndHandler(_("button_unpublish", default="Unpublish"))
-    def handleUnpublish(self, action):
-        published_survey = self.context
-        wt = api.portal.get_tool("portal_workflow")
-        if wt.getInfoFor(published_survey, "review_state") != "published":
-            logger.warning(
-                "Trying to unpublish survey %s which is not marked as published",
-                "/".join(published_survey.getPhysicalPath()),
-            )
-        else:
-            wt.doActionFor(published_survey, "retract")
-        notify(SurveyUnpublishEvent(published_survey))
-        self.request.response.redirect(self.nextURL())
-
-    @button.buttonAndHandler(_("button_cancel", default="Cancel"))
-    def handleCancel(self, action):
-        self.request.response.redirect(self.nextURL())

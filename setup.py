8b1b19ed4f6b687d5a27fafc0f15d4956bdfebc0
Johannes Raggam <thetetet@gmail.com>
2025-01-13 14:42:56 +0100

    Remove support and testing infrastructure for Plone 5.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import monkeypatch_setup  # noqa: F401
 import os
 
 
-version = "10.0.6.dev0"
+version = "10.1.0.dev0"
 
 setup(
     name="osha.oira",
@@ -18,7 +18,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python :: 3",

1fc35f97ac02104c91cbb5fcc2782eb74842fbc3
Cillian de Róiste <deroiste@syslab.com>
2025-05-14 17:52:17 +0100

    Add @survey-publication-count restapi service
    Refs: https://github.com/syslabcom/scrum/issues/3488

---
+++ news/349.added
@@ -0,0 +1,1 @@
+Add @survey-publication-count restapi service. This returns the data from @@survey-publication-count as JSON.
---
+++ src/osha/oira/services/survey_publication_count.py
@@ -0,0 +1,16 @@
+from plone import api
+from plone.restapi.services import Service
+
+
+class SurveyPublicationCountService(Service):
+    """A JSON service version of @@survey-publication-count"""
+
+    def reply(self):
+        view = api.content.get_view(
+            "survey-publication-count", self.context, self.request
+        )
+        view()
+        return {
+            "@id": self.request.getURL(),
+            "items": view.rows,
+        }

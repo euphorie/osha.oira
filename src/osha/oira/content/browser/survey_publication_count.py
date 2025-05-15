69f7e3f93efd4c5c069cfceadd9552780f5d60c7
Cillian de Róiste <deroiste@syslab.com>
2025-05-15 11:10:12 +0100

    @@survey-publication-count: Move function into the view class
    As recommended by Ale, to make it easier to reuse and customize
    
    Refs: https://github.com/syslabcom/scrum/issues/3453

--- src/osha/oira/content/browser/survey_publication_count.py
+++ src/osha/oira/content/browser/survey_publication_count.py
@@ -5,52 +5,45 @@ from Products.Five import BrowserView
 import csv
 
 
-fieldnames = ["country", "path", "count"]
-
-
-def get_survey_details():
-    details = []
-    pc = api.portal.get_tool("portal_catalog")
-    wft = api.portal.get_tool("portal_workflow")
-    surveys = pc.searchResults(portal_type="euphorie.survey")
-    for brain in surveys:
-        survey = brain.getObject()
-        review_history = wft.getInfoFor(survey, "review_history")
-        count = 0
-        for entry in review_history:
-            if entry["action"] in ["publish", "update"]:
-                count += 1
-        if count:
-            split_path = brain.getPath().split("/")
-            country = split_path[3]
-            details.append(
-                {
-                    "country": country,
-                    "path": "/".join(split_path[2:]),
-                    "count": count,
-                }
-            )
-    sorted_details = sorted(details, key=lambda r: r["country"])
-    return sorted_details
-
-
 class SurveyPublicationCount(BrowserView):
     """Report the number of times each Survey has been published"""
 
-    def __call__(self):
-        self.fieldnames = fieldnames
-        self.rows = get_survey_details()
-        return self.index()
+    fieldnames = ["country", "path", "count"]
+
+    def get_survey_details(self):
+        details = []
+        pc = api.portal.get_tool("portal_catalog")
+        wft = api.portal.get_tool("portal_workflow")
+        surveys = pc.searchResults(portal_type="euphorie.survey")
+        for brain in surveys:
+            survey = brain.getObject()
+            review_history = wft.getInfoFor(survey, "review_history")
+            count = 0
+            for entry in review_history:
+                if entry["action"] in ["publish", "update"]:
+                    count += 1
+            if count:
+                split_path = brain.getPath().split("/")
+                country = split_path[3]
+                details.append(
+                    {
+                        "country": country,
+                        "path": "/".join(split_path[2:]),
+                        "count": count,
+                    }
+                )
+        sorted_details = sorted(details, key=lambda r: r["country"])
+        return sorted_details
 
 
-class SurveyPublicationCountCSV(BrowserView):
+class SurveyPublicationCountCSV(SurveyPublicationCount):
     """CSV download version of SurveyPublicationCount"""
 
     def __call__(self):
         csvfile = StringIO()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
         writer.writeheader()
-        for row in get_survey_details():
+        for row in self.get_survey_details():
             writer.writerow(row)
         csvfile.seek(0)
         self.request.response.setHeader("Content-type", "text/csv")

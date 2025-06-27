e8e77f73fc631301d0e2649704e9f26ee8831381
Cillian de Róiste <deroiste@syslab.com>
2025-06-27 12:05:49 +0100

    @@upload: raise BadRequest if the request is missing the file
    Refs: https://github.com/syslabcom/scrum/issues/3489

--- src/osha/oira/services/upload.py
+++ src/osha/oira/services/upload.py
@@ -2,23 +2,22 @@ from lxml.etree import XMLSyntaxError
 from plone import api
 from plone.restapi.deserializer import json_body
 from plone.restapi.services import Service
+from zExceptions import BadRequest
 
 import base64
 
 
 class Upload(Service):
-    """A JSON service version of @@upload"""
+    """A JSON service version of @upload"""
 
     def reply(self):
         data = json_body(self.request)
         upload = data.get("upload", None)
         if upload is None:
-            self.request.response.setStatus(400)
-
+            raise BadRequest("Missing XML file")
         surveygroup_title = data.get("surveygroup_title", "")
         survey_title = data.get("survey_title", "OiRA Tool import")
         is_etranslate_compatible = data.get("is_etranslate_compatible", False)
-            return {"error": {"type": "Missing xml file"}}
         upload_view = api.content.get_view("upload", self.context, self.request)
         importer = upload_view.importer_factory(self.context)
         try:

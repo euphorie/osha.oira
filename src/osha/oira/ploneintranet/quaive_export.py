8cfe9b8614288b2b2a475dcae87366319c0b4974
Maurits van Rees <maurits@vanrees.org>
2025-08-28 10:57:35 +0200

    Add quaive-export-survey view.
    Refs https://github.com/syslabcom/scrum/issues/3604

---
+++ src/osha/oira/ploneintranet/quaive_export.py
@@ -0,0 +1,15 @@
+from euphorie.content.browser.export import ExportSurvey as EuphorieExportSurvey
+from osha.oira.ploneintranet.interfaces import IQuaiveForm
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.i18nmessageid import MessageFactory
+from zope.interface import implementer
+
+
+_ = MessageFactory("nuplone")
+
+
+@implementer(IQuaiveForm)
+class ExportSurvey(EuphorieExportSurvey):
+    template = ViewPageTemplateFile("templates/quaive-form.pt")
+    label = _("Export OiRA Tool")
+    description = ""

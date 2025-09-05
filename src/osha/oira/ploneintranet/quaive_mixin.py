6780c45548e40644353b756533218e19486600fd
Maurits van Rees <maurits@vanrees.org>
2025-09-05 11:49:16 +0200

    quaive-form.pt: add view.form_classes to the form html classes.
    Moved `QuaiveCreateFormMixin` from `quaive_create.py` to `quaive_mixin.py`, where `QuaiveEditFormMixin` already was.
    Introduced a `QuaiveFormMixin` in that file, and let both other mixin classes inherit from it.
    Let `QuaiveExportSurvey` inherit from this as well.

--- src/osha/oira/ploneintranet/quaive_mixin.py
+++ src/osha/oira/ploneintranet/quaive_mixin.py
@@ -1,10 +1,24 @@
 from osha.oira.ploneintranet.interfaces import IQuaiveForm
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.interface import alsoProvides
 from zope.interface import implementer
 
 
 @implementer(IQuaiveForm)
-class QuaiveEditFormMixin:
+class QuaiveFormMixin:
+
+    form_classes = "pat-inject"
+
+    def update(self):
+        super().update()
+        for group in getattr(self, "groups", []):
+            #  This is needed to pick up the custom Quaive widgets
+            alsoProvides(group, IQuaiveForm)
+
+
+@implementer(IQuaiveForm)
+class QuaiveEditFormMixin(QuaiveFormMixin):
+
     @property
     def template(self):
         return self.index
@@ -21,8 +35,11 @@ class QuaiveEditFormMixin:
         """Is this the main edit form or is it some other form?"""
         return self.__name__ == "quaive-edit"
 
-    def update(self):
-        super().update()
-        for group in getattr(self, "groups", []):
-            #  This is needed to pick up the custom Quaive widgets
-            alsoProvides(group, IQuaiveForm)
+
+@implementer(IQuaiveForm)
+class QuaiveCreateFormMixin(QuaiveFormMixin):
+    template = ViewPageTemplateFile("templates/quaive-panel-form.pt")
+
+    @property
+    def oira_type(self):
+        return self.__name__.rpartition("-")[-1]
